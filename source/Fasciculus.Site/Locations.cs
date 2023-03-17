c49ba41a005973ec6dd06cc93a1cff93851aa6b3
rhjoerg <rhjoerg@gmail.com>
2023-03-17 21:34:43 +0100

    Initial docu (#2)

---
+++ source/Fasciculus.Site/Locations.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace Fasciculus.Site
+{
+    public static class Locations
+    {
+        private static readonly FileInfo AssemblyFile
+            = new(typeof(Locations).Assembly.Location);
+
+        public static readonly DirectoryInfo ProjectDirectory
+            = AssemblyFile.Directory!.Parent!.Parent!.Parent!;
+
+        public static readonly DirectoryInfo OutputDirectory
+            = new(Path.Combine(ProjectDirectory.Parent!.Parent!.Parent!.FullName, "fasciculus.github.io"));
+    }
+}
---
+++ source/Fasciculus.Site/Program.cs
@@ -0,0 +1,21 @@
+﻿using Statiq.App;
+using Statiq.Common;
+using Statiq.Web;
+using System.Threading.Tasks;
+
+namespace Fasciculus.Site
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            int result = await Bootstrapper
+                .Factory
+                .CreateWeb(args)
+                .SetOutputPath(Locations.OutputDirectory.FullName)
+                .RunAsync();
+
+            return result;
+        }
+    }
+}
