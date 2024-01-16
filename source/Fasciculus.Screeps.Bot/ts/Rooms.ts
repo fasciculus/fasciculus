275b5e157ead581420c405d7e1162d64e14bd722
rhjoerg <rhjoerg@gmail.com>
2024-01-16 16:04:59 +0100

    Added Rooms.my

--- source/Fasciculus.Screeps.Bot/ts/Rooms.ts
+++ source/Fasciculus.Screeps.Bot/ts/Rooms.ts
@@ -4,11 +4,21 @@ import * as _ from "lodash";
 export class Rooms
 {
     private static _all: Room[] = [];
+    private static _my: Room[] = [];
+
+    static get all(): Room[] { return Rooms._all; }
+    static get my(): Room[] { return Rooms._my; }
+
+    static isMyRoom(room: Room): boolean
+    {
+        var controller = room.controller;
+
+        return (controller !== undefined) && controller.my;
+    }
 
     static initialize()
     {
         Rooms._all = _.values(Game.rooms);
+        Rooms._my = Rooms._all.filter(Rooms.isMyRoom);
     }
-
-    static get all(): Room[] { return Rooms._all; }
 }
--- source/Fasciculus.Screeps.Bot/ts/main.ts
+++ source/Fasciculus.Screeps.Bot/ts/main.ts
@@ -1,6 +1,7 @@
 import { Initializer } from "./Initializer"
 import { Memories } from "./Memories";
 import { Names } from "./Names";
+import { Rooms } from "./Rooms";
 import { Scheduler } from "./Scheduler";
 import { Spawns } from "./Spawns";
 
@@ -8,6 +9,4 @@ export const loop = function ()
 {
     Initializer.run();
     Scheduler.run();
-
-    console.log(Spawns.my);
 }
