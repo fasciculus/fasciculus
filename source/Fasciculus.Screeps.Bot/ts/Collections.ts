e0ebd926a9a7fb66dcd2bd5eeb033af92c776a82
rhjoerg <rhjoerg@gmail.com>
2024-01-26 13:48:06 +0100

    added PriorityQueue

--- source/Fasciculus.Screeps.Bot/ts/Collections.ts
+++ source/Fasciculus.Screeps.Bot/ts/Collections.ts
@@ -274,3 +274,69 @@ export class Dictionaries
         return Object.assign({}, dictionary);
     }
 }
+
+export type PriorityQueueCompare<T> = (a: T, b: T) => number;
+
+interface PriorityQueueEntry<T>
+{
+    value: T;
+    next?: PriorityQueueEntry<T>;
+}
+
+export class PriorityQueue<T>
+{
+    readonly compare: PriorityQueueCompare<T>
+
+    private first?: PriorityQueueEntry<T>;
+
+    constructor(compare: PriorityQueueCompare<T>)
+    {
+        this.compare = compare;
+    }
+
+    push(value: T)
+    {
+        if (this.first)
+        {
+            let compare: PriorityQueueCompare<T> = this.compare;
+
+            if (compare(value, this.first.value) < 0)
+            {
+                this.first = { value, next: this.first };
+            }
+            else
+            {
+                let prev: PriorityQueueEntry<T> = this.first;
+                let next: PriorityQueueEntry<T> | undefined = prev.next;
+
+                while (next && compare(next.value, value) < 0)
+                {
+                    prev = next;
+                    next = prev.next;
+                }
+
+                prev.next = { value, next };
+            }
+        }
+        else
+        {
+            this.first = { value };
+        }
+    }
+
+    pop(): T | undefined
+    {
+        if (this.first)
+        {
+            let value = this.first.value;
+
+            this.first = this.first.next;
+
+            return value;
+        }
+        else
+        {
+            return undefined;
+        }
+    }
+}
