5b50952701e1254ca45b391a1e4fc8283297c467
rhjoerg <rhjoerg@gmail.com>
2024-01-28 02:17:37 +0100

    auto creation of Config.ts

--- source/Fasciculus.Experiments.Build/Program.cs
+++ source/Fasciculus.Experiments.Build/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace Fasciculus.Experiments.Build
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Fasciculus.Experiments.Build
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load("Config.xml");
+
+            XmlNodeList adds = doc.GetElementsByTagName("Add");
+            List<string> lines = new List<string>();
+
+            foreach (var add in adds)
+            {
+                XmlElement element = (XmlElement)add;
+                string key = element.GetAttribute("key");
+                string value = element.InnerText;
+
+                lines.Add($"export const {key} = {value};");
+            }
+
+            string profilerSession = Guid.NewGuid().ToString("N");
+
+            lines.Add($"export const PROFILER_SESSION = \"{profilerSession}\";");
+
+            File.WriteAllLines("Config.ts", lines);
         }
     }
 }
---
+++ source/Fasciculus.Screeps.Bot/ts/Config.ts
@@ -0,0 +1,10 @@
+export const PROFILER_SESSION = "ce3a3b83d4324dcd8777a843fadbf66e";
+export const PROFILER_WARMUP = 10;
+export const PROFILER_LOG_INTERVAL = 10;
+export const PROFILER_MAX_ENTRIES = 10;
+export const PROFILER_IGNORED_KEYS = ["Wellers:prepare", "Builders:execute"];
+export const SUPPLIER_ENOUGH_ENERGY_RATIO = 0.75;
+export const SUPPLIER_PERFORMANCE_FACTOR = 0.6;
+export const SUPPLIER_MIN_SUPPLY_ENERGY = 10;
+export const SUPPLIER_MIN_CREEP_FREE_RATIO = 0.8;
+export const SUPPLIER_MAX_NEW_ASSIGNMENTS = 2;
