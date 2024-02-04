bdb00b9966f41a25bde83d880d15ffc099383092
rhjoerg <rhjoerg@gmail.com>
2024-02-04 20:25:48 +0100

    replacing Dictionary with Map

--- source/Fasciculus.Screeps.Bot/ts/Infrastructure.ts
+++ source/Fasciculus.Screeps.Bot/ts/Infrastructure.ts
@@ -100,9 +100,9 @@ export class Wall
 
 export class Walls
 {
-    private static _myWalls: Dictionary<Wall> = {};
+    private static _myWalls: Map<WallId, Wall> = new Map();
 
-    static get my(): Vector<Wall> { return Dictionaries.values(Walls._myWalls); }
+    static get my(): Vector<Wall> { return new Vector(Walls._myWalls.vs()); }
     static get avg(): number { return Walls.my.avg(w => w.hits); }
 
     static get newest(): Vector<Wall> { return Walls.my.filter(w => w.hits == 1); }
@@ -110,7 +110,7 @@ export class Walls
     @profile
     static initialize()
     {
-        Dictionaries.update(Walls._myWalls, Chambers.myWalls, id => new Wall(id as WallId));
+        Walls._myWalls.update(Chambers.myWalls, id => new Wall(id));
     }
 
     static get myWeakest(): Wall | undefined
