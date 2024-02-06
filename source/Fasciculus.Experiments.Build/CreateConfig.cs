19170951891a7791084da076e0a4d4fc6d343b12
rhjoerg <rhjoerg@gmail.com>
2024-02-06 01:49:31 +0100

    improved Config.ts creation

---
+++ source/Fasciculus.Experiments.Build/CreateConfig.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Build.Utilities;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+
+namespace Fasciculus.Experiments.Build
+{
+    public class CreateConfig : Task
+    {
+        public override bool Execute()
+        {
+            XmlDocument doc = new XmlDocument();
+
+            doc.Load("Config.xml");
+
+            XmlNodeList adds = doc.GetElementsByTagName("Add");
+            List<string> lines = new List<string>();
+
+            foreach (var add in adds)
+            {
+                XmlElement element = (XmlElement)add;
+                string key = element.GetAttribute("key");
+                string value = element.InnerText;
+                lines.Add("export const " + key + " = " + value + ";");
+            }
+
+            File.WriteAllLines("ts/Config.ts", lines);
+
+            return true;
+        }
+    }
+}
