f1809c877bb0263368114815c03b5954e00f93e7
rhjoerg <rhjoerg@gmail.com>
2024-02-08 16:57:29 +0100

    added main entry to generated package.json

--- source/fasciculus-package/Gruntfile.js
+++ source/fasciculus-package/Gruntfile.js
@@ -6,12 +6,12 @@ module.exports = function (grunt)
     grunt.config.init(
         {
             pkg: pkg,
+            package: { dest: "dist", main: "tasks/package.js" },
             copy:
             {
                 dist: { files: [{ expand: true, cwd: "src", src: ["**", "!package-lock.json"], dest: "dist" }] },
                 install: { files: [{ expand: true, cwd: "dist", src: ["**", "!package-lock.json"], dest: '../../npm_modules/' + pkg["name"] }] }
-            },
-            package: { dest: "dist", main: "tasks/package.js" }
+            }
         });
 
     grunt.task.loadTasks("src/tasks");
