2871de17fbf5defbce7b64f1bd38e27e21283625
rhjoerg <rhjoerg@gmail.com>
2024-02-09 09:56:03 +0100

    renamed ws5 to es2015

---
+++ source/fasciculus-es2015/Gruntfile.js
@@ -0,0 +1,23 @@
+
+module.exports = function (grunt)
+{
+    var pkg = grunt.file.readJSON("package.json");
+    var dest = "../../npm_modules/" + pkg["name"];
+
+    grunt.config.init(
+        {
+            pkg: pkg,
+            concat: { default: { src: "src/*.ts", dest: "build/fasciculus-es2015.ts" } },
+            ts: { default: { tsconfig: './tsconfig.json' } },
+            package: { default: { dest: "dist/" } },
+            copy: { default: { files: [{ cwd: "dist", src: ["package.json", "*.js"], dest, expand: true }] } },
+        }
+    );
+
+    grunt.task.loadNpmTasks("grunt-contrib-concat");
+    grunt.task.loadNpmTasks("grunt-ts");
+    grunt.task.loadNpmTasks("fasciculus-package");
+    grunt.task.loadNpmTasks("grunt-contrib-copy");
+
+    grunt.task.registerTask("default", ["concat", "ts", "package", "copy"]);
+}
