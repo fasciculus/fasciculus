bb354c71435e64a5d9454e522858fe75399ada35
rhjoerg <rhjoerg@gmail.com>
2024-02-09 05:56:02 +0100

    implemented Objects utility

--- source/fasciculus-utils-types/src/fasciculus-utils.d.ts
+++ source/fasciculus-utils-types/src/fasciculus-utils.d.ts
@@ -2,4 +2,11 @@
 interface Objects
 {
     keys(o: {} | object | undefined | null): Set<string>;
+    values<T>(o: { [s: string]: T; } | undefined | null): Array<T>;
+
+    setFunction<T>(target: T, key: PropertyKey, fn: Function): T;
+    setGetter<T>(target: T, key: PropertyKey, fn: () => any): T;
+    setValue<T>(target: T, key: PropertyKey, value: any | undefined): T
 }
+
+declare var Objects: Objects;
