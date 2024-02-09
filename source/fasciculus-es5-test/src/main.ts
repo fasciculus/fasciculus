5a6bb87d244fe253365a576f16723cb886eb7e64
rhjoerg <rhjoerg@gmail.com>
2024-02-09 17:44:29 +0100

    to be removed

--- source/fasciculus-es5-test/src/main.ts
+++
@@ -1,8 +0,0 @@
-
-import "fasciculus-es5";
-
-var input: Array<number | undefined> = [1, 2, undefined, 3];
-var output: Array<number> = Array.defined(input);
-
-console.log(`input = ${input}`);
-console.log(`output = ${output}`);
