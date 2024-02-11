82cda75e69442895e2f57a7011bbc2c8f4dcae50
rhjoerg <rhjoerg@gmail.com>
2024-02-11 13:45:47 +0100

    added BodyTemplate

--- source/fasciculus-bot/src/screeps/body.ts
+++ source/fasciculus-bot/src/screeps/body.ts
@@ -1,4 +1,78 @@
 
+type BodyChunk = { cost: number; parts: Array<BodyPartConstant>; };
+
+export class BodyTemplate
+{
+    private static _priorities: {[part: string]: number} =
+    {
+        "tough": 1,
+        "work": 2,
+        "attack": 3,
+        "ranged_attack": 4,
+        "carry": 5,
+        "move": 6,
+        "heal": 7,
+        "claim": 8
+    };
+
+    private _chunks: Array<BodyChunk> = new Array();
+
+    add(times: number, ...parts: Array<BodyPartConstant>): BodyTemplate
+    {
+        const cost = BodyTemplate.cost(...parts);
+        const chunk: BodyChunk = { cost, parts };
+
+        for (var i = 0; i < times; ++i)
+        {
+            this._chunks.push(chunk);
+        }
+
+        return this;
+    }
+
+    static createTemplate(times: number, ...parts: Array<BodyPartConstant>): BodyTemplate
+    {
+        return new BodyTemplate().add(times, ...parts);
+    }
+
+    static cost(...parts: Array<BodyPartConstant>): number
+    {
+        return parts.sum(p => BODYPART_COST[p]);
+    }
+
+    createBody(energy: number): Array<BodyPartConstant> | undefined
+    {
+        const chunkCount: number = this.chunkCount(energy);
+
+        if (chunkCount == 0) return undefined;
+
+        return Array.flatten(this._chunks.take(chunkCount).map(c => c.parts)).sort(BodyTemplate.compare)
+    }
+
+    private chunkCount(energy: number): number
+    {
+        var result: number = 0;
+
+        for (let chunk of this._chunks)
+        {
+            if (energy < chunk.cost) break;
+
+            ++result;
+            energy -= chunk.cost;
+        }
+
+        return result;
+    }
+
+    private static compare(a: BodyPartConstant, b: BodyPartConstant)
+    {
+        let pa: number = BodyTemplate._priorities[a] || 99;
+        let pb: number = BodyTemplate._priorities[b] || 99;
+
+        return pa - pb;
+    }
+}
+
 export class Bodies
 {
     private static _infos: Map<CreepId, BodyInfo> = new Map();
