0c445a0563d3ce482fbed959126b44634429923c
rhjoerg <rhjoerg@gmail.com>
2024-07-13 13:22:18 +0200

    Added Folders.Documents

---
+++ source/Fasciculus.Core/IO/Folders.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace Fasciculus.IO
+{
+    public static class Folders
+    {
+        public static DirectoryInfo Documents
+        {
+            get
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    return new(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
+                }
+
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
