3bd1329db77cb50a0cc3b5ad3bd5dd73fe9cb3cc
rhjoerg <rhjoerg@gmail.com>
2024-07-13 13:00:55 +0200

    added SdeExtractor

---
+++ source/Fasciculus.Eve/IO/SdeExtractor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fasciculus.Eve.IO
+{
+    public class SdeExtractor
+    {
+        public static void Run()
+        {
+
+        }
+    }
+}
--- source/Fasciculus.Eve/Program.cs
+++ source/Fasciculus.Eve/Program.cs
@@ -1,9 +1,11 @@
-﻿namespace Fasciculus.Eve;
+﻿using Fasciculus.Eve.IO;
+
+namespace Fasciculus.Eve;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        SdeExtractor.Run();
     }
 }
