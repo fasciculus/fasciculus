20cb035383f216e03b0a83d227b5cfbebfb85fd8
rhjoerg <rhjoerg@gmail.com>
2024-07-14 11:11:58 +0200

    replaced Folders with Directories

---
+++ source/Fasciculus.Core/IO/Directories.cs
@@ -0,0 +1,10 @@
+﻿using System.IO;
+
+namespace Fasciculus.IO
+{
+    public static class Directories
+    {
+        public static DirectoryInfo Personal => new(Paths.Personal);
+        public static DirectoryInfo Documents => new(Paths.Documents);
+    }
+}
