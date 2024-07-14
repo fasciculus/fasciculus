f1e2e3517515a2a22f79a93c05093170d304bc43
rhjoerg <rhjoerg@gmail.com>
2024-07-14 11:12:27 +0200

    added DirectoryInfoExtensions.Combine

---
+++ source/Fasciculus.Core/IO/DirectoryInfoExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.IO;
+
+namespace Fasciculus.IO
+{
+    public static class DirectoryInfoExtensions
+    {
+        public static DirectoryInfo Combine(this DirectoryInfo directory, params string[] paths)
+        {
+            string result = directory.FullName;
+
+            foreach (string path in paths)
+            {
+                result = Path.Combine(result, path);
+            }
+
+            return new DirectoryInfo(result);
+        }
+    }
+}
