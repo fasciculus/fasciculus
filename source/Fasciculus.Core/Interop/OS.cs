49310aac3c180c676e59363a3b52d9c63bcc9be2
rhjoerg <rhjoerg@gmail.com>
2024-07-14 11:11:05 +0200

    replaced OSTypes with OS

---
+++ source/Fasciculus.Core/Interop/OS.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Fasciculus.Interop
+{
+    public static class OS
+    {
+        public static bool IsWindows
+        {
+            get => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
+        }
+    }
+}
