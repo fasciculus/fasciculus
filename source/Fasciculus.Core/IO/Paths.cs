cfc536bd7cae85074d8c6fb1cf725909a7b31ab8
rhjoerg <rhjoerg@gmail.com>
2024-07-14 11:11:22 +0200

    added Paths

---
+++ source/Fasciculus.Core/IO/Paths.cs
@@ -0,0 +1,11 @@
+﻿using Fasciculus.Interop;
+using static System.Environment;
+
+namespace Fasciculus.IO
+{
+    public static class Paths
+    {
+        public static string Personal = GetFolderPath(SpecialFolder.Personal);
+        public static string Documents = GetFolderPath(OS.IsWindows ? SpecialFolder.MyDocuments : SpecialFolder.Personal);
+    }
+}
