8ea58cedc4918379fe8e43da2b08e5f07bf201d7
rhjoerg <rhjoerg@gmail.com>
2024-07-15 20:33:50 +0200

    moved DirectoryInfoExtensions to System.IO

--- source/Fasciculus.Core/IO/DirectoryInfoExtensions.cs
+++ source/Fasciculus.Core/IO/DirectoryInfoExtensions.cs
@@ -1,26 +1,7 @@
-﻿using System.IO;
-
-namespace Fasciculus.IO
+﻿namespace System.IO
 {
     public static class DirectoryInfoExtensions
     {
-        public static DirectoryInfo Combine(this DirectoryInfo directory, params string[] paths)
-        {
-            string result = directory.FullName;
-
-            foreach (string path in paths)
-            {
-                result = Path.Combine(result, path);
-            }
-
-            return new DirectoryInfo(result);
-        }
-
-        public static FileInfo File(this DirectoryInfo directory, string name)
-        {
-            return new(Path.Combine(directory.FullName, name));
-        }
-
         public static DirectoryInfo Existing(this DirectoryInfo directory, bool create = true)
         {
             DirectoryInfo result = new(directory.FullName);
@@ -39,6 +20,23 @@ namespace Fasciculus.IO
             }
 
             return result;
+        }
+
+        public static DirectoryInfo Combine(this DirectoryInfo directory, params string[] paths)
+        {
+            string result = directory.FullName;
+
+            foreach (string path in paths)
+            {
+                result = Path.Combine(result, path);
+            }
+
+            return new DirectoryInfo(result);
+        }
+
+        public static FileInfo File(this DirectoryInfo directory, string name)
+        {
+            return new(Path.Combine(directory.FullName, name));
         }
     }
 }
