ccc42c78294bb28dc9473b47750af74945f4d1c6
rhjoerg <rhjoerg@gmail.com>
2024-07-17 12:22:26 +0200

    added Data.WriteInt and Data.WriteString

--- source/Fasciculus.Core/IO/Data.cs
+++ source/Fasciculus.Core/IO/Data.cs
@@ -22,6 +22,13 @@ namespace Fasciculus.IO
             return BitConverter.ToInt32(buffer, 0);
         }
 
+        public void WriteInt(int value)
+        {
+            byte[] bytes = BitConverter.GetBytes(value);
+
+            WriteBytes(bytes, 4);
+        }
+
         public string ReadString()
         {
             int length = ReadInt();
@@ -32,9 +39,23 @@ namespace Fasciculus.IO
             return Encoding.UTF8.GetString(bytes, 0, length);
         }
 
+        public void WriteString(string value)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(value);
+            int length = bytes.Length;
+
+            WriteInt(length);
+            WriteBytes(bytes, length);
+        }
+
         private void ReadBytes(byte[] bytes, int length)
         {
             stream.Read(bytes, 0, length);
+        }
+
+        private void WriteBytes(byte[] bytes, int length)
+        {
+            stream.Write(bytes, 0, length);
         }
     }
 }
