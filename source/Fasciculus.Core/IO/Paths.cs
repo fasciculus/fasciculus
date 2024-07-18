f9172600e3fd4ccbe0acf7c9b565e1198a25219f
rhjoerg <rhjoerg@gmail.com>
2024-07-18 12:17:00 +0200

    added Paths.Downloads and Directories.Downloads

--- source/Fasciculus.Core/IO/Paths.cs
+++ source/Fasciculus.Core/IO/Paths.cs
@@ -1,11 +1,18 @@
 ﻿using Fasciculus.Interop;
+using System.IO;
 using static System.Environment;
 
 namespace Fasciculus.IO
 {
     public static class Paths
     {
-        public static string Personal = GetFolderPath(SpecialFolder.Personal);
-        public static string Documents = GetFolderPath(OS.IsWindows ? SpecialFolder.MyDocuments : SpecialFolder.Personal);
+        public static string Personal =>
+            GetFolderPath(SpecialFolder.Personal);
+
+        public static string Documents =>
+            GetFolderPath(OS.IsWindows ? SpecialFolder.MyDocuments : SpecialFolder.Personal);
+
+        public static string Downloads =>
+            OS.IsWindows ? Path.Combine(Personal, "Downloads") : Personal;
     }
 }
