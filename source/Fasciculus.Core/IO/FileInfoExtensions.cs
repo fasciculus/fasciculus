e2267e55f032d9881e0249d5ed97c36806e82e82
rhjoerg <rhjoerg@gmail.com>
2024-07-18 19:57:25 +0200

    added FileInfoExtensions.Write

--- source/Fasciculus.Core/IO/FileInfoExtensions.cs
+++ source/Fasciculus.Core/IO/FileInfoExtensions.cs
@@ -4,6 +4,14 @@ namespace System.IO
 {
     public static class FileInfoExtensions
     {
+        public static void DeleteIfExists(this FileInfo file)
+        {
+            if (File.Exists(file.FullName))
+            {
+                file.Delete();
+            }
+        }
+
         public static bool IsNewerThan(this FileInfo file, FileInfo target)
             => !target.Exists || file.LastWriteTimeUtc > target.LastWriteTimeUtc;
 
@@ -12,5 +20,17 @@ namespace System.IO
 
         public static string ReadAllText(this FileInfo file)
             => File.ReadAllText(file.FullName, Encoding.UTF8);
+
+        public static FileInfo Write(this FileInfo file, Action<Stream> write)
+        {
+            file.DeleteIfExists();
+
+            using (Stream stream = file.Create())
+            {
+                write(stream);
+            }
+
+            return new(file.FullName);
+        }
     }
 }
