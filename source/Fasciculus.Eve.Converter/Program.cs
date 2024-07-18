eb7c27ab14972395214fa903959e8d181d20706e
rhjoerg <rhjoerg@gmail.com>
2024-07-18 12:24:53 +0200

    added Fasciculus.Eve.Converter

---
+++ source/Fasciculus.Eve.Converter/Program.cs
@@ -0,0 +1,13 @@
+﻿using Fasciculus.IO;
+using System;
+
+namespace Fasciculus.Eve
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(Directories.Downloads);
+        }
+    }
+}
