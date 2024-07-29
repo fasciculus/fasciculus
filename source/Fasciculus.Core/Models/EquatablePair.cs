9c4370a163628bb90663158c93f3cc764a35ae74
rhjoerg <rhjoerg@gmail.com>
2024-07-29 13:09:19 +0200

    added Pair

---
+++ source/Fasciculus.Core/Models/EquatablePair.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fasciculus.Models
+{
+    public class EquatablePair<T1, T2> : Pair<T1, T2>, IEquatable<Pair<T1, T2>>
+        where T1 : IEquatable<T1>
+        where T2 : IEquatable<T2>
+    {
+        public EquatablePair(T1 first, T2 second)
+            : base(first, second) { }
+
+        public bool Equals(Pair<T1, T2> other)
+            => First.Equals(other.First) && Second.Equals(other.Second);
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Pair<T1, T2> other) return Equals(other);
+
+            return false;
+        }
+
+        public override int GetHashCode()
+            => base.GetHashCode();
+
+        public static bool operator ==(EquatablePair<T1, T2> a, EquatablePair<T1, T2> b)
+            => a.Equals(b);
+
+        public static bool operator !=(EquatablePair<T1, T2> a, EquatablePair<T1, T2> b)
+            => !a.Equals(b);
+    }
+}
---
+++ source/Fasciculus.Core/Models/Pair.cs
@@ -0,0 +1,20 @@
+﻿namespace Fasciculus.Models
+{
+    public class Pair<T1, T2>
+    {
+        public readonly T1 First;
+        public readonly T2 Second;
+
+        public Pair(T1 first, T2 second)
+        {
+            First = first;
+            Second = second;
+        }
+
+        public override int GetHashCode()
+            => First.GetHashCode() + Second.GetHashCode();
+
+        public override string ToString()
+            => $"({First}, {Second})";
+    }
+}
