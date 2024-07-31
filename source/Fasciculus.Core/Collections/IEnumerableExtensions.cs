3acc2427dfd59c5053566daad990fcb872250ed0
rhjoerg <rhjoerg@gmail.com>
2024-08-01 01:27:08 +0200

    added IEnumerableExtensions.Apply

---
+++ source/Fasciculus.Core/Collections/IEnumerableExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace System.Collections.Generic
+{
+    public static class IEnumerableExtensions
+    {
+        public static void Apply<T>(this IEnumerable<T> values, Action<T> action)
+        {
+            foreach (var value in values)
+            {
+                action(value);
+            }
+        }
+    }
+}
