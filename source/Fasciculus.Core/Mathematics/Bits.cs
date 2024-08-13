581b09601b21998fa10fe7069e7a5f5204384844
rhjoerg <rhjoerg@gmail.com>
2024-08-13 12:37:59 +0200

    added Bits

---
+++ source/Fasciculus.Core/Mathematics/Bits.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Fasciculus.Mathematics
+{
+    public static class Bits
+    {
+        private static readonly int[] counts = GetCounts(0x10000);
+
+        public static int Count(byte value)
+            => counts[value];
+
+        public static int Count(IEnumerable<byte> values)
+            => values.Select(Count).Sum();
+
+        public static int[] GetCounts(int length)
+        {
+            int[] counts = new int[length];
+
+            for (int i = 0; i < length; ++i)
+            {
+                int count = 0;
+                int x = i;
+
+                while (x > 0)
+                {
+                    x &= x - 1;
+                    ++count;
+                }
+
+                counts[i] = count;
+            }
+
+            return counts;
+        }
+    }
+}
