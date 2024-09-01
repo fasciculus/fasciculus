c8511e4a9f67b7089624daeef7d92e66be21c9a7
rhjoerg <rhjoerg@gmail.com>
2024-09-01 13:00:01 +0200

    added Fasciculus.Experiments.MAUI

---
+++ source/Fasciculus.Experiments.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Fasciculus.Experiments.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ source/Fasciculus.Experiments.MAUI/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Fasciculus.Experiments.MAUI
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
