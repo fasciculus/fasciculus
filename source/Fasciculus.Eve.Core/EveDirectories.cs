c14d8a2bf768305db5eb90a13e97ff4029e90f5d
rhjoerg <rhjoerg@gmail.com>
2024-09-15 12:44:37 +0200

    added EveDirectories

---
+++ source/Fasciculus.Eve.Core/EveDirectories.cs
@@ -0,0 +1,10 @@
+﻿using Fasciculus.IO;
+using System.IO;
+
+namespace Fasciculus.Eve
+{
+    public static class EveDirectories
+    {
+        public static DirectoryInfo Documents => Directories.Documents.Combine("Fasciculus", "Eve").Existing();
+    }
+}
