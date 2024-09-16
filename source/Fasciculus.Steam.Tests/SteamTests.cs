6728976f0c9bdcf4cf3d1f3b09355583d9ea4c3b
rhjoerg <rhjoerg@gmail.com>
2024-09-16 12:32:34 +0200

    added Fasciculus.Steam

---
+++ source/Fasciculus.Steam.Tests/SteamTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fasciculus.Steam.Tests
+{
+    [TestClass]
+    public class SteamTests
+    {
+        [TestMethod]
+        public void Test()
+        {
+
+        }
+    }
+}
