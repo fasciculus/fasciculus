6049ec17824b637eaaed480c71b79d22322eb306
rhjoerg <rhjoerg@gmail.com>
2024-09-16 12:54:53 +0200

    registry experiments

--- source/Fasciculus.Steam.Tests/SteamTests.cs
+++ source/Fasciculus.Steam.Tests/SteamTests.cs
@@ -1,4 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Win32;
+using System;
+using System.Runtime.Versioning;
 
 namespace Fasciculus.Steam.Tests
 {
@@ -6,9 +9,26 @@ namespace Fasciculus.Steam.Tests
     public class SteamTests
     {
         [TestMethod]
+        [SupportedOSPlatform("windows")]
         public void Test()
         {
+            using RegistryKey? software = Registry.CurrentUser.OpenSubKey("SOFTWARE", false);
+            using RegistryKey? valve = software?.OpenSubKey("Valve", false);
+            using RegistryKey? steam = valve?.OpenSubKey("Steam", false);
+            using RegistryKey? apps = steam?.OpenSubKey("Apps", false);
 
+            string[] ids = apps?.GetSubKeyNames() ?? [];
+
+            foreach (string id in ids)
+            {
+                using RegistryKey? app = apps?.OpenSubKey(id, false);
+                string? name = app?.GetValue("Name")?.ToString();
+
+                if (name is not null)
+                {
+                    Console.WriteLine(name);
+                }
+            }
         }
     }
 }
