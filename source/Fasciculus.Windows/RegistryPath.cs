ac5912d3a934d30ae4fadd5f30e39730acf17ce3
rhjoerg <rhjoerg@gmail.com>
2024-10-01 07:31:07 +0200

    added Fasciculus.Windows

---
+++ source/Fasciculus.Windows/RegistryPath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Microsoft.Win32
+{
+    public class RegistryPath
+    {
+        private readonly string[] names;
+
+        public RegistryHive Hive { get; }
+        public IEnumerable<string> Names { get { return names; } }
+
+        public RegistryPath(RegistryHive hive, params string[] names)
+        {
+            Hive = hive;
+            this.names = [.. names];
+        }
+    }
+}
