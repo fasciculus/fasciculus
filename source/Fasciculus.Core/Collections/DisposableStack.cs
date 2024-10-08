db10d06cf666a4ff46ec54d0b2fbac2a94d5659d
rhjoerg <rhjoerg@gmail.com>
2024-10-08 11:46:14 +0200

    added DisposableStack

---
+++ source/Fasciculus.Core/Collections/DisposableStack.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Fasciculus.Collections
+{
+    public class DisposableStack<T> : Stack<T>, IDisposable
+        where T : notnull, IDisposable
+    {
+        public DisposableStack() { }
+        public DisposableStack(IEnumerable<T> collection) : base(collection) { }
+        public DisposableStack(int capacity) : base(capacity) { }
+
+        ~DisposableStack()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            while (Count > 0)
+            {
+                Pop().Dispose();
+            }
+        }
+    }
+}
