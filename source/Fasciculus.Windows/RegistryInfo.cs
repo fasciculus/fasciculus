6378c52508dc2282d869f8cc512fb51627d195b4
rhjoerg <rhjoerg@gmail.com>
2024-10-11 08:13:14 +0200

    added RegistryValue

--- source/Fasciculus.Windows/RegistryInfo.cs
+++ source/Fasciculus.Windows/RegistryInfo.cs
@@ -8,11 +8,13 @@ namespace Fasciculus.Windows
     public class RegistryInfo
     {
         private readonly RegistryPath[] children;
+        private readonly Dictionary<string, RegistryValue> values;
 
         public RegistryPath Path { get; }
         public bool Exists { get; }
 
         public IEnumerable<RegistryPath> Children => children;
+        public IReadOnlyDictionary<string, RegistryValue> Values => values;
 
         public RegistryInfo(RegistryPath path)
         {
@@ -42,10 +44,12 @@ namespace Fasciculus.Windows
             if (exists)
             {
                 children = parent.GetSubKeyNames().Select(name => RegistryPath.Combine(path, name)).ToArray();
+                values = parent.GetValueNames().Select(name => RegistryValue.Create(parent, name)).ToDictionary(rk => rk.Name);
             }
             else
             {
                 children = [];
+                values = [];
             }
         }
     }
---
+++ source/Fasciculus.Windows/RegistryValue.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Win32;
+using System;
+
+namespace Fasciculus.Windows
+{
+    public class RegistryValue
+    {
+        public string Name { get; }
+        public RegistryValueKind Kind { get; }
+
+        private string? stringValue;
+        private uint? uintValue;
+
+        public string StringValue => stringValue ?? string.Empty;
+
+        protected RegistryValue(string name, RegistryValueKind kind)
+        {
+            Name = name;
+            Kind = kind;
+        }
+
+        protected RegistryValue(string name, RegistryValueKind kind, string value)
+        {
+            Name = name;
+            Kind = kind;
+            stringValue = value;
+        }
+
+        protected RegistryValue(string name, RegistryValueKind kind, uint value)
+        {
+            Name = name;
+            Kind = kind;
+            uintValue = value;
+        }
+
+        public static RegistryValue Create(RegistryKey key, string name)
+        {
+            RegistryValueKind kind = key.GetValueKind(name);
+            object? value = key.GetValue(name);
+
+            switch (kind)
+            {
+                case RegistryValueKind.None: return new(name, kind);
+                case RegistryValueKind.Unknown: return new(name, kind);
+                case RegistryValueKind.String: return new(name, kind, Convert.ToString(value));
+                case RegistryValueKind.ExpandString: return new(name, kind, Convert.ToString(value));
+                case RegistryValueKind.Binary: return new(name, kind);
+                case RegistryValueKind.DWord: return new(name, kind, Convert.ToUInt32(value));
+                case RegistryValueKind.MultiString: return new(name, kind);
+                case RegistryValueKind.QWord: return new(name, kind);
+            }
+
+            return new(name, kind);
+        }
+    }
+}
