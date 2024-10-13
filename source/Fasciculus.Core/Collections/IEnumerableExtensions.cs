1054a587f815463a5aa3510f63de4f80cadb1a93
rhjoerg <rhjoerg@gmail.com>
2024-10-13 13:15:10 +0200

    added IEnumerableExtensions.NotNull

--- source/Fasciculus.Core/Collections/IEnumerableExtensions.cs
+++ source/Fasciculus.Core/Collections/IEnumerableExtensions.cs
@@ -1,7 +1,13 @@
-﻿namespace System.Collections.Generic
+﻿using System.Linq;
+
+namespace System.Collections.Generic
 {
     public static class IEnumerableExtensions
     {
+        public static IEnumerable<T> NotNull<T>(this IEnumerable<T?> collection)
+            where T : notnull
+            => collection.Where(x => x is not null).Cast<T>();
+
         public static void Apply<T>(this IEnumerable<T> values, Action<T> action)
         {
             foreach (var value in values)
