a091800acf28f53a4e570faedb5a3a814c369261
rhjoerg <rhjoerg@gmail.com>
2024-10-17 13:13:27 +0200

    added Zip.Overwrite

--- source/Fasciculus.Core/IO/Zip.cs
+++ source/Fasciculus.Core/IO/Zip.cs
@@ -1,22 +1,32 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace Fasciculus.IO
 {
     public static class Zip
     {
-        public static async Task Extract(FileInfo zipFile, DirectoryInfo outputDirectory)
+        public enum Overwrite
+        {
+            Never,
+            Always,
+            Changed
+        }
+
+        public static async Task Extract(FileInfo zipFile, DirectoryInfo outputDirectory, Overwrite overwrite)
         {
             using Stream stream = zipFile.OpenRead();
-            using ZipArchive archive = new ZipArchive(stream, ZipArchiveMode.Read);
+            using ZipArchive archive = new(stream, ZipArchiveMode.Read);
 
-            foreach (ZipArchiveEntry entry in archive.Entries)
+            IEnumerable<ZipArchiveEntry> entries = archive.Entries.Where(e => Filter(e, outputDirectory, overwrite));
+
+            foreach (ZipArchiveEntry entry in entries)
             {
                 FileInfo outputFile = outputDirectory.File(entry.FullName);
 
-                if (outputFile.Exists) continue;
-
+                outputFile.DeleteIfExists();
                 outputFile.Directory.Create();
 
                 using Stream entryStream = entry.Open();
@@ -24,6 +34,25 @@ namespace Fasciculus.IO
 
                 await entryStream.CopyToAsync(outputStream);
             }
+        }
+
+        private static bool Filter(ZipArchiveEntry entry, DirectoryInfo outputDirectory, Overwrite overwrite)
+        {
+            if (overwrite == Overwrite.Always) return true;
+
+            FileInfo outputFile = outputDirectory.File(entry.FullName);
+
+            if (outputFile.Exists)
+            {
+                if (overwrite == Overwrite.Never) return false;
+
+                if (entry.LastWriteTime.UtcDateTime > outputFile.LastWriteTimeUtc) return true;
+                if (entry.Length != outputFile.Length) return true;
+
+                return false;
+            }
+
+            return true;
         }
     }
 }
