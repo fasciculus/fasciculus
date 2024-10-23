3f6e8a3a5e6505dfe8c5878767b9b099d6d76bf7
rhjoerg <rhjoerg@gmail.com>
2024-10-23 09:35:27 +0200

    added Cond

---
+++ source/Fasciculus.Core/Validating/Cond.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Fasciculus.Validating
+{
+    public static class Cond
+    {
+        public static T NotNull<T>(T? value)
+            where T : notnull
+        {
+            return value ?? throw new InvalidOperationException();
+        }
+    }
+}
