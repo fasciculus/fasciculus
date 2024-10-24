9b6e9f16a007069a527a5690a4e49c7d1151b444
rhjoerg <rhjoerg@gmail.com>
2024-10-24 17:00:47 +0200

    cleanup

--- source/Fasciculus.Core/Mathematics/SparseMatrixInt.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Fasciculus.Mathematics
-{
-    public class SparseMatrixInt : SparseMatrix<int>
-    {
-        public SparseMatrixInt(int[] offsets, int[] columns, int[] values)
-            : base(offsets, columns, values) { }
-    }
-}
