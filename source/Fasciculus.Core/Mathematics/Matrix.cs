b687af092f8ff570b288c5c1faa8bca8c8d96fbc
rhjoerg <rhjoerg@gmail.com>
2024-10-24 18:02:24 +0200

    refactoring

--- source/Fasciculus.Core/Mathematics/Matrix.cs
+++ source/Fasciculus.Core/Mathematics/Matrix.cs
@@ -1,11 +1,40 @@
 ﻿namespace Fasciculus.Mathematics
 {
-    public class Matrix<T> : IMatrix<T>
+    public interface IMatrix<T>
     {
+        public int RowCount { get; }
+        public int ColumnCount { get; }
     }
 
-    public class MutableMatrix<T> : Matrix<T>, IMutableMatrix<T>
+    public interface IMutableMatrix<T> : IMatrix<T>
     {
+        public void Set(int row, int col, T value);
+    }
 
+    public class MutableDenseIntMatrix : IMutableMatrix<int>
+    {
+        private readonly int[] values;
+
+        public int RowCount { get; }
+        public int ColumnCount { get; }
+
+        public MutableDenseIntMatrix(int rowCount, int columnCount)
+        {
+            values = new int[rowCount * columnCount];
+
+            RowCount = rowCount;
+            ColumnCount = columnCount;
+        }
+
+        public void Set(int row, int col, int value)
+        {
+            values[row * ColumnCount + col] = value;
+        }
+    }
+
+    public static class Matrices
+    {
+        public static IMutableMatrix<int> CreateMutableDenseInt(int rows, int cols)
+            => new MutableDenseIntMatrix(rows, cols);
     }
 }
