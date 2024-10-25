c3dd74c11b83ecd284a47e2f863d45fd94b3a7bf
rhjoerg <rhjoerg@gmail.com>
2024-10-25 17:11:41 +0200

    added Fasciculus.Core.Tests

---
+++ source/Fasciculus.Core.Tests/Mathematics/MatrixTests.cs
@@ -0,0 +1,45 @@
+﻿using Fasciculus.Mathematics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fasciculus.Core.Tests.Mathematics
+{
+    [TestClass]
+    public class MatrixTests
+    {
+        [TestMethod]
+        public void TestBoolMatrix()
+        {
+            IMutableMatrix<bool> m1 = Matrices.CreateMutableSparseBool(3, 3);
+
+            m1.Set(0, 0, true);
+            m1.Set(1, 1, true);
+            m1.Set(2, 2, true);
+
+            Assert.IsTrue(m1.Get(0, 0));
+            Assert.IsFalse(m1.Get(0, 1));
+            Assert.IsFalse(m1.Get(0, 2));
+
+            Assert.IsFalse(m1.Get(1, 0));
+            Assert.IsTrue(m1.Get(1, 1));
+            Assert.IsFalse(m1.Get(1, 2));
+
+            Assert.IsFalse(m1.Get(2, 0));
+            Assert.IsFalse(m1.Get(2, 1));
+            Assert.IsTrue(m1.Get(2, 2));
+
+            IMatrix<bool> m2 = m1.ToMatrix();
+
+            Assert.IsTrue(m2.Get(0, 0));
+            Assert.IsFalse(m2.Get(0, 1));
+            Assert.IsFalse(m2.Get(0, 2));
+
+            Assert.IsFalse(m2.Get(1, 0));
+            Assert.IsTrue(m2.Get(1, 1));
+            Assert.IsFalse(m2.Get(1, 2));
+
+            Assert.IsFalse(m2.Get(2, 0));
+            Assert.IsFalse(m2.Get(2, 1));
+            Assert.IsTrue(m2.Get(2, 2));
+        }
+    }
+}
---
+++ source/Fasciculus.Core.Tests/MiscTests.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Fasciculus.Core.Tests
+{
+    [TestClass]
+    public class MiscTests
+    {
+        [TestMethod]
+        public void TestBinarySearch()
+        {
+            int[] values = { 1, 3, 5, 7 };
+
+            Assert.AreEqual(0, Array.BinarySearch(values, 0, 4, 1));
+        }
+    }
+}
