eaa0341cc908d5a2d3df45adb0d6e4d24aacddb2
rhjoerg <rhjoerg@gmail.com>
2024-10-25 17:10:15 +0200

    added Ex.NotImplemented

---
+++ source/Fasciculus.Core/Validating/Ex.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Fasciculus.Validating
+{
+    public static class Ex
+    {
+        public static NotImplementedException NotImplemented()
+            => new();
+    }
+}
