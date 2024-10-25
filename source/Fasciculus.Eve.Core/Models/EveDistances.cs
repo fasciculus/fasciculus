4f8b5740b5a7850b4bd299b82c4723fa6179933a
rhjoerg <rhjoerg@gmail.com>
2024-10-25 17:41:19 +0200

    added EveDistances.CreateConnections

--- source/Fasciculus.Eve.Core/Models/EveDistances.cs
+++ source/Fasciculus.Eve.Core/Models/EveDistances.cs
@@ -1,4 +1,6 @@
 ﻿using Fasciculus.Mathematics;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Fasciculus.Eve.Models
 {
@@ -13,9 +15,27 @@ namespace Fasciculus.Eve.Models
 
         public static EveDistances Create(IEveUniverse universe, double minSecurity)
         {
+            IMatrix<bool> connections = CreateConnections(universe, minSecurity);
             IMutableMatrix<int> distances = InitializeDistances(universe);
 
             return new(distances);
+        }
+
+        private static IMatrix<bool> CreateConnections(IEveUniverse universe, double minSecurity)
+        {
+            EveSolarSystems solarSystems = universe.SolarSystems;
+            IMutableMatrix<bool> connections = Matrices.CreateMutableSparseBool(solarSystems.Count, solarSystems.Count);
+
+            foreach (EveSolarSystem origin in solarSystems)
+            {
+                IEnumerable<EveSolarSystem> destinations = origin.Stargates
+                    .Select(stargate => stargate.Destination.SolarSystem)
+                    .Where(destination => destination.Security >= minSecurity);
+
+                destinations.Apply(destination => connections.Set(origin.Index, destination.Index, true));
+            }
+
+            return connections.ToMatrix();
         }
 
         private static IMutableMatrix<int> InitializeDistances(IEveUniverse universe)
