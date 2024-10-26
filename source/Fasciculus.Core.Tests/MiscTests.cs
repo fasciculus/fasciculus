cf60a771df8f3195975ba7932f19fff3f83040c0
rhjoerg <rhjoerg@gmail.com>
2024-10-26 13:06:42 +0200

    added MiscTests.TestArraySort

--- source/Fasciculus.Core.Tests/MiscTests.cs
+++ source/Fasciculus.Core.Tests/MiscTests.cs
@@ -13,5 +13,17 @@ namespace Fasciculus.Core.Tests
 
             Assert.AreEqual(0, Array.BinarySearch(values, 0, 4, 1));
         }
+
+        [TestMethod]
+        public void TestArraySort()
+        {
+            int[] values = { 4, 2, 3, 1 };
+
+            Array.Sort(values);
+
+            Assert.IsTrue(values[0] < values[1]);
+            Assert.IsTrue(values[1] < values[2]);
+            Assert.IsTrue(values[2] < values[3]);
+        }
     }
 }
