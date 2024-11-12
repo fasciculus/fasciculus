cb9b8033b4511a04d55260a28b7eee643124f9e0
rhjoerg <rhjoerg@gmail.com>
2024-11-12 23:43:09 +0100

    added RomanNumbers

---
+++ source/Fasciculus.Core.Tests/Algorithms/RomanNumbersTests.cs
@@ -0,0 +1,24 @@
+﻿using Fasciculus.Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Fasciculus.Core.Tests.Algorithms
+{
+    [TestClass]
+    public class RomanNumbersTests
+    {
+        [TestMethod]
+        public void TestFormat()
+        {
+            Assert.AreEqual("I", RomanNumbers.Format(1));
+            Assert.AreEqual("II", RomanNumbers.Format(2));
+            Assert.AreEqual("III", RomanNumbers.Format(3));
+            Assert.AreEqual("IV", RomanNumbers.Format(4));
+            Assert.AreEqual("V", RomanNumbers.Format(5));
+            Assert.AreEqual("VI", RomanNumbers.Format(6));
+            Assert.AreEqual("VII", RomanNumbers.Format(7));
+            Assert.AreEqual("VIII", RomanNumbers.Format(8));
+            Assert.AreEqual("IX", RomanNumbers.Format(9));
+            Assert.AreEqual("X", RomanNumbers.Format(10));
+        }
+    }
+}
---
+++ source/Fasciculus.Core/Algorithms/RomanNumbers.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace Fasciculus.Algorithms
+{
+    public static class RomanNumbers
+    {
+        private static readonly string[] romans = { "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" };
+        private static readonly List<int> values = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+
+        public static string Format(int value)
+        {
+            StringBuilder result = new();
+
+            while (value > 0)
+            {
+                int index = values.FindIndex(x => x <= value);
+
+                value -= values[index];
+                result.Append(romans[index]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
