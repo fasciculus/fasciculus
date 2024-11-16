53cd0a91af3cb157d36edc175d53d866e96d40d7
rhjoerg <rhjoerg@gmail.com>
2024-11-16 02:34:42 +0100

    cleanup

--- source/Fasciculus.Core/Threading/TaskExtensions.cs
+++ source/Fasciculus.Core/Threading/TaskExtensions.cs
@@ -1,17 +1,8 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-
-namespace System.Threading.Tasks
+﻿namespace System.Threading.Tasks
 {
     public static class TaskExtensions
     {
         public static T Run<T>(this Task<T> task)
             => task.GetAwaiter().GetResult();
-
-        public static async Task WaitAll(this IEnumerable<Task> tasks)
-        {
-            Task.WaitAll(tasks.ToArray());
-            await Task.CompletedTask;
-        }
     }
 }
