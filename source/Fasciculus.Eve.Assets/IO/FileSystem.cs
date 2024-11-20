5d0a1f73c37230b323a58e338d7c9b6b1a041586
rhjoerg <rhjoerg@gmail.com>
2024-11-20 11:43:30 +0100

    added AssetDirectories and AssetFiles

---
+++ source/Fasciculus.Eve.Assets/IO/FileSystem.cs
@@ -0,0 +1,41 @@
+﻿using Fasciculus.IO;
+using System.IO;
+
+namespace Fasciculus.Eve.IO
+{
+    public interface IAssetDirectories
+    {
+        public DirectoryInfo Documents { get; }
+        public DirectoryInfo Downloads { get; }
+    }
+
+    public class AssetDirectories : IAssetDirectories
+    {
+        private readonly ISpecialDirectories specialDirectories;
+
+        public DirectoryInfo Documents => specialDirectories.Documents.Combine("Fasciculus", "Eve.Assets").CreateIfNotExists();
+        public DirectoryInfo Downloads => Documents.Combine("Downloads").CreateIfNotExists();
+
+        public AssetDirectories(ISpecialDirectories specialDirectories)
+        {
+            this.specialDirectories = specialDirectories;
+        }
+    }
+
+    public interface IAssetFiles
+    {
+        public FileInfo SdeZip { get; }
+    }
+
+    public class AssetFiles : IAssetFiles
+    {
+        private readonly IAssetDirectories assetDirectories;
+
+        public FileInfo SdeZip => assetDirectories.Downloads.File("sde.zip");
+
+        public AssetFiles(IAssetDirectories assetDirectories)
+        {
+            this.assetDirectories = assetDirectories;
+        }
+    }
+}
