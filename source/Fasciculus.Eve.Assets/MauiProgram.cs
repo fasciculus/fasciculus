f1635be987cf44312dc874182ea7d0fd6a7f73ff
rhjoerg <rhjoerg@gmail.com>
2024-11-23 01:50:51 +0100

    refactored MauiProgram

--- source/Fasciculus.Eve.Assets/MauiProgram.cs
+++ source/Fasciculus.Eve.Assets/MauiProgram.cs
@@ -7,19 +7,29 @@ namespace Fasciculus.Eve.Assets
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-                });
 
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
+            builder.UseMauiApp<App>();
+
+            ConfigureFonts(builder);
+            ConfigureLogging(builder);
 
             return builder.Build();
+        }
+
+        private static void ConfigureFonts(MauiAppBuilder builder)
+        {
+            builder.ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+        }
+
+        private static void ConfigureLogging(MauiAppBuilder builder)
+        {
+#if DEBUG
+            builder.Logging.AddDebug();
+#endif
         }
     }
 }
