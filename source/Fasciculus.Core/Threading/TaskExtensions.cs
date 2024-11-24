9241b8f11f6008465d6589dd8a5d7d9b2964e068
rhjoerg <rhjoerg@gmail.com>
2024-11-24 21:54:49 +0100

    cleanup

---
+++ source/Fasciculus.Core/Threading/TaskExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace System.Threading.Tasks
+{
+    public static class TaskExtensions
+    {
+        public static void Run(this Task task)
+            => task.GetAwaiter().GetResult();
+
+        public static T Run<T>(this Task<T> task)
+            => task.GetAwaiter().GetResult();
+    }
+}
