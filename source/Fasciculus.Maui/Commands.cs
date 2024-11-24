e33d49054a98bef0abcf3e1fcea8425ad9f9a63d
rhjoerg <rhjoerg@gmail.com>
2024-11-24 21:55:42 +0100

    added Fasciculus.Maui

---
+++ source/Fasciculus.Maui/Commands.cs
@@ -0,0 +1,50 @@
+﻿using Fasciculus.Threading;
+using Microsoft.Maui.ApplicationModel;
+using System;
+using System.Windows.Input;
+
+namespace Fasciculus.Maui
+{
+    public class LongRunningCommand : ICommand
+    {
+        private readonly Action action;
+
+        private bool canExecute = true;
+        private readonly ReentrantTaskSafeMutex mutex = new();
+
+        public event EventHandler? CanExecuteChanged;
+
+        public LongRunningCommand(Action action)
+        {
+            this.action = action;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            using Locker locker = Locker.Lock(mutex);
+
+            return canExecute;
+        }
+
+        public void Execute(object? parameter)
+        {
+            using Locker locker = Locker.Lock(mutex);
+
+            if (canExecute)
+            {
+                SetCanExecute(false);
+
+                Tasks.LongRunning(action).ContinueWith((t) => { SetCanExecute(true); });
+            }
+        }
+
+        private void SetCanExecute(bool value)
+        {
+            using Locker locker = Locker.Lock(mutex);
+
+            canExecute = value;
+
+            MainThread.BeginInvokeOnMainThread(() => CanExecuteChanged?.Invoke(this, new EventArgs()));
+        }
+    }
+}
