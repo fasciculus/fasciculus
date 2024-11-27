e4274df4be358a4485a0a6a6571f47bdc5ee1b99
rhjoerg <rhjoerg@gmail.com>
2024-11-27 01:02:33 +0100

    set dark theme

--- source/Fasciculus.Eve.Assets/MainPage.xaml.cs
+++ source/Fasciculus.Eve.Assets/MainPage.xaml.cs
@@ -1,24 +1,10 @@
-﻿using System.ComponentModel;
-
-namespace Fasciculus.Eve.Assets
+﻿namespace Fasciculus.Eve.Assets
 {
     public partial class MainPage : ContentPage
     {
-        private readonly MainPageModel model;
-
         public MainPage(MainPageModel model)
         {
             InitializeComponent();
-            BindingContext = model;
-
-            this.model = model;
-
-            // model.PropertyChanged += ModelPropertyChanged;
-        }
-
-        private void ModelPropertyChanged(object? sender, PropertyChangedEventArgs e)
-        {
-            BindingContext = null;
             BindingContext = model;
         }
     }
