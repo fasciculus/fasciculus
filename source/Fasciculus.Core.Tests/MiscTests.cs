b13de7ba3592248c6fac561a6457b9f746799789
rhjoerg <rhjoerg@gmail.com>
2024-11-29 09:23:34 +0100

    added TaskAndThreadTests

--- source/Fasciculus.Core.Tests/MiscTests.cs
+++ source/Fasciculus.Core.Tests/MiscTests.cs
@@ -1,72 +1,14 @@
-﻿using Fasciculus.Algorithms;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Diagnostics;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Fasciculus.Core.Tests
 {
     [TestClass]
     public class MiscTests : TestsBase
     {
+        const int TASK_COUNT = 50;
         [TestMethod]
-        public unsafe void Test()
+        public void Test()
         {
-            int[] array = { 1, 2, 3, 4, 5, 6, 7 };
-            int reps = 1_000_000;
-
-            Stopwatch sw1 = new();
-            Stopwatch sw2 = new();
-
-            sw1.Start();
-
-            for (int i = 0; i < reps; ++i)
-            {
-                BinarySearch.IndexOf(array, i);
-            }
-
-            sw1.Stop();
-
-            sw2.Start();
-
-            for (int i = 0; i < reps; ++i)
-            {
-                IndexOf(array, i);
-            }
-
-            sw2.Stop();
-
-            Log($"spn: {sw1.Elapsed}");
-            Log($"ptr: {sw2.Elapsed}");
-        }
-
-        private static unsafe int IndexOf(int[] sorted, int value)
-        {
-            int lo = 0;
-            int hi = sorted.Length - 1;
-
-            fixed (int* a = sorted)
-            {
-                while (lo <= hi)
-                {
-                    int i = lo + ((hi - lo) >> 1);
-                    int x = a[i];
-
-                    if (x == value)
-                    {
-                        return i;
-                    }
-
-                    if (x < value)
-                    {
-                        lo = i + 1;
-                    }
-                    else
-                    {
-                        hi = i - 1;
-                    }
-                }
-            }
-
-            return -1;
         }
     }
 }
