88f24c3ec2ec09d29a1a27968ba32a2374b9ed03
rhjoerg <rhjoerg@gmail.com>
2024-12-08 05:26:48 +0100

    removed PendingToDoneConverter

--- source/Fasciculus.Eve.Assets/Pages/Converters.cs
+++ source/Fasciculus.Eve.Assets/Pages/Converters.cs
@@ -44,44 +44,6 @@ namespace Fasciculus.Eve.Assets.Pages
         }
     }
 
-    public class PendingToDoneConverter : IValueConverter
-    {
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            if (value is PendingToDone status)
-            {
-                if (targetType == typeof(string))
-                {
-                    return status switch
-                    {
-                        PendingToDone.Pending => "Pending",
-                        PendingToDone.Working => "Working",
-                        PendingToDone.Done => "Done",
-                        _ => string.Empty
-                    };
-                }
-
-                if (targetType == typeof(Color))
-                {
-                    return status switch
-                    {
-                        PendingToDone.Pending => Colors.Orange,
-                        PendingToDone.Working => Colors.Orange,
-                        PendingToDone.Done => Colors.Green,
-                        _ => Colors.Red
-                    };
-                }
-            }
-
-            return null;
-        }
-
-        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
-        {
-            return null;
-        }
-    }
-
     public class LongProgressInfoConverter : IValueConverter
     {
         public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
