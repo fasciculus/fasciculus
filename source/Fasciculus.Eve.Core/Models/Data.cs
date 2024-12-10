50f7795f126e24c82d8fa66547be851f8593f338
rhjoerg <rhjoerg@gmail.com>
2024-12-10 08:47:52 +0100

    improved EvePlanetSchematics

--- source/Fasciculus.Eve.Core/Models/Data.cs
+++ source/Fasciculus.Eve.Core/Models/Data.cs
@@ -309,6 +309,15 @@ namespace Fasciculus.Eve.Models
         }
     }
 
+    public enum EvePlanetSchematicLevel
+    {
+        P0,
+        P1,
+        P2,
+        P3,
+        P4
+    }
+
     [DebuggerDisplay("{Name}")]
     public class EvePlanetSchematic
     {
@@ -359,8 +368,12 @@ namespace Fasciculus.Eve.Models
 
         private readonly EvePlanetSchematicType[] inputs;
         public IReadOnlyList<EvePlanetSchematicType> Inputs => inputs;
+        public IEnumerable<EveType> InputTypes => Inputs.Select(x => x.Type);
 
         public EvePlanetSchematicType Output { get; }
+        public EveType OutputType => Output.Type;
+
+        public EvePlanetSchematicLevel Level { get; internal set; } = EvePlanetSchematicLevel.P0;
 
         public EvePlanetSchematic(Data data, EveTypes types)
         {
@@ -375,9 +388,44 @@ namespace Fasciculus.Eve.Models
     {
         private readonly EvePlanetSchematic[] planetSchematics;
 
+        private readonly EveType[] inputTypes;
+        private readonly EveType[] outputTypes;
+
+        private readonly EveType[] p0;
+        private readonly EvePlanetSchematic[] p1;
+        private readonly EvePlanetSchematic[] p2;
+        private readonly EvePlanetSchematic[] p3;
+        private readonly EvePlanetSchematic[] p4;
+
+        public IReadOnlyList<EveType> InputTypes => inputTypes;
+        public IReadOnlyList<EveType> OutputTypes => outputTypes;
+
+        public IReadOnlyList<EveType> P0 => p0;
+        public IReadOnlyList<EvePlanetSchematic> P1 => p1;
+        public IReadOnlyList<EvePlanetSchematic> P2 => p2;
+        public IReadOnlyList<EvePlanetSchematic> P3 => p3;
+        public IReadOnlyList<EvePlanetSchematic> P4 => p4;
+
         public EvePlanetSchematics(IEnumerable<EvePlanetSchematic> planetSchematics)
         {
             this.planetSchematics = planetSchematics.ToArray();
+
+            inputTypes = FetchInputTypes(this.planetSchematics);
+            outputTypes = FetchOutputTypes(this.planetSchematics);
+
+            p0 = FetchP0(inputTypes, outputTypes);
+
+            p1 = FetchP1(this.planetSchematics, p0);
+            p1.Apply(x => { x.Level = EvePlanetSchematicLevel.P1; });
+
+            p2 = FetchP2(this.planetSchematics, p0, p1);
+            p2.Apply(x => { x.Level = EvePlanetSchematicLevel.P2; });
+
+            p3 = FetchP3(this.planetSchematics, p0, p1, p2);
+            p3.Apply(x => { x.Level = EvePlanetSchematicLevel.P3; });
+
+            p4 = FetchP4(this.planetSchematics, p0, p1, p2, p3);
+            p4.Apply(x => { x.Level = EvePlanetSchematicLevel.P4; });
         }
 
         public IEnumerator<EvePlanetSchematic> GetEnumerator()
@@ -385,6 +433,61 @@ namespace Fasciculus.Eve.Models
 
         IEnumerator IEnumerable.GetEnumerator()
             => planetSchematics.GetEnumerator();
+
+        private static EveType[] FetchInputTypes(EvePlanetSchematic[] planetSchematics)
+            => [.. planetSchematics.SelectMany(x => x.InputTypes).Distinct().OrderBy(x => x.Id)];
+
+        private static EveType[] FetchOutputTypes(EvePlanetSchematic[] planetSchematics)
+            => [.. planetSchematics.Select(x => x.OutputType).OrderBy(x => x.Id)];
+
+        private static EveType[] FetchP0(EveType[] inputTypes, EveType[] outputTypes)
+        {
+            bool filter(EveType x) => !outputTypes.Contains(x);
+
+            return [.. inputTypes.Where(filter).OrderBy(x => x.Id)];
+        }
+
+        private static EvePlanetSchematic[] FetchP1(EvePlanetSchematic[] planetSchematics, EveType[] p0)
+        {
+            bool filter(EvePlanetSchematic x) => x.InputTypes.All(t => p0.Contains(t));
+
+            return [.. planetSchematics.Where(filter).OrderBy(x => x.Id)];
+        }
+
+        private static EvePlanetSchematic[] FetchP2(EvePlanetSchematic[] planetSchematics, EveType[] p0, EvePlanetSchematic[] p1)
+        {
+            EveType[] t1 = [.. p1.Select(x => x.OutputType)];
+            bool filter1(EvePlanetSchematic x) => x.InputTypes.Any(t => t1.Contains(t));
+            bool filter01(EvePlanetSchematic x) => x.InputTypes.All(t => p0.Contains(t) || t1.Contains(t));
+            bool filter(EvePlanetSchematic x) => filter1(x) && filter01(x);
+
+            return [.. planetSchematics.Where(filter).OrderBy(x => x.Id)];
+        }
+
+        private static EvePlanetSchematic[] FetchP3(EvePlanetSchematic[] planetSchematics, EveType[] p0, EvePlanetSchematic[] p1,
+            EvePlanetSchematic[] p2)
+        {
+            EveType[] t1 = [.. p1.Select(x => x.OutputType)];
+            EveType[] t2 = [.. p2.Select(x => x.OutputType)];
+            bool filter2(EvePlanetSchematic x) => x.InputTypes.Any(t => t2.Contains(t));
+            bool filter012(EvePlanetSchematic x) => x.InputTypes.All(t => p0.Contains(t) || t1.Contains(t) || t2.Contains(t));
+            bool filter(EvePlanetSchematic x) => filter2(x) && filter012(x);
+
+            return [.. planetSchematics.Where(filter).OrderBy(x => x.Id)];
+        }
+
+        private static EvePlanetSchematic[] FetchP4(EvePlanetSchematic[] planetSchematics, EveType[] p0, EvePlanetSchematic[] p1,
+            EvePlanetSchematic[] p2, EvePlanetSchematic[] p3)
+        {
+            EveType[] t1 = [.. p1.Select(x => x.OutputType)];
+            EveType[] t2 = [.. p2.Select(x => x.OutputType)];
+            EveType[] t3 = [.. p3.Select(x => x.OutputType)];
+            bool filter3(EvePlanetSchematic x) => x.InputTypes.Any(t => t3.Contains(t));
+            bool filter0123(EvePlanetSchematic x) => x.InputTypes.All(t => p0.Contains(t) || t1.Contains(t) || t2.Contains(t) || t3.Contains(t));
+            bool filter(EvePlanetSchematic x) => filter3(x) && filter0123(x);
+
+            return [.. planetSchematics.Where(filter).OrderBy(x => x.Id)];
+        }
     }
 
     public class EveData
--- source/Fasciculus.Eve.Core/Services/PlanetaryIndustry.cs
+++ source/Fasciculus.Eve.Core/Services/PlanetaryIndustry.cs
@@ -1,4 +1,5 @@
-﻿using Fasciculus.Threading;
+﻿using Fasciculus.Eve.Models;
+using Fasciculus.Threading;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using System.Threading.Tasks;
@@ -14,6 +15,13 @@ namespace Fasciculus.Eve.Services
     {
         private readonly TaskSafeMutex mutex = new();
 
+        private readonly IEveResources resources;
+
+        public PlanetaryIndustry(IEveResources resources)
+        {
+            this.resources = resources;
+        }
+
         public Task StartAsync()
         {
             using Locker locker = Locker.Lock(mutex);
@@ -23,7 +31,18 @@ namespace Fasciculus.Eve.Services
 
         private void Start()
         {
+            EvePlanetSchematics schematics = GetSchematics();
 
+            EveType[] p0 = [.. schematics.P0];
+            EvePlanetSchematic[] p1 = [.. schematics.P1];
+            EvePlanetSchematic[] p2 = [.. schematics.P2];
+            EvePlanetSchematic[] p3 = [.. schematics.P3];
+            EvePlanetSchematic[] p4 = [.. schematics.P4];
+        }
+
+        private EvePlanetSchematics GetSchematics()
+        {
+            return Tasks.Wait(resources.Data).PlanetSchematics;
         }
     }
 
