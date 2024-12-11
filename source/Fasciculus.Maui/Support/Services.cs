ace9dec88a24be1bb194bd8689bd5be07d6a7a97
rhjoerg <rhjoerg@gmail.com>
2024-12-11 07:48:24 +0100

    added MauiAppServices

---
+++ source/Fasciculus.Maui/Support/Services.cs
@@ -0,0 +1,26 @@
+﻿using Fasciculus.Support;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Maui.Hosting;
+using System;
+
+namespace Fasciculus.Maui.Support
+{
+    public static class MauiAppServices
+    {
+        private static IServiceProvider? services;
+
+        public static MauiApp InitializeServices(this MauiApp mauiApp)
+        {
+            services = mauiApp.Services;
+
+            return mauiApp;
+        }
+
+        public static T GetRequiredService<T>()
+            where T : notnull
+
+        {
+            return Cond.NotNull(services).GetRequiredService<T>();
+        }
+    }
+}
