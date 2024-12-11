ce71d113695ff3dce89d4ff8029e44b32baabe6f
rhjoerg <rhjoerg@gmail.com>
2024-12-11 21:18:33 +0100

    fixed WorkStateLabel

--- source/Fasciculus.Maui/Controls/WorkStateLabel.cs
+++ source/Fasciculus.Maui/Controls/WorkStateLabel.cs
@@ -8,74 +8,47 @@ namespace Fasciculus.Maui.Controls
     {
         public static readonly BindableProperty SourceProperty
             = BindableProperty.Create(nameof(Source), typeof(WorkState), typeof(WorkStateLabel), null,
-                BindingMode.OneWay, null, OnSourcePropertyChanged);
+                BindingMode.OneWay, null, OnWorkStateLabelPropertyChanged);
 
         public static readonly BindableProperty PendingColorProperty
             = BindableProperty.Create(nameof(PendingColor), typeof(Color), typeof(WorkStateLabel), null,
-                BindingMode.OneWay, null, null);
+                BindingMode.OneWay, null, OnWorkStateLabelPropertyChanged);
 
         public static readonly BindableProperty WorkingColorProperty
             = BindableProperty.Create(nameof(WorkingColor), typeof(Color), typeof(WorkStateLabel), null,
-                BindingMode.OneWay, null, null);
+                BindingMode.OneWay, null, OnWorkStateLabelPropertyChanged);
 
         public static readonly BindableProperty DoneColorProperty
             = BindableProperty.Create(nameof(DoneColor), typeof(Color), typeof(WorkStateLabel), null,
-                BindingMode.OneWay, null, null);
+                BindingMode.OneWay, null, OnWorkStateLabelPropertyChanged);
 
-        private static void OnSourcePropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
+        private static void OnWorkStateLabelPropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
         {
             if (bindable is WorkStateLabel label)
             {
-                if (newvalue is WorkState state)
-                {
-                    SetTextAndColor(label, state);
-                }
+                label.SetTextAndColor();
             }
         }
 
-        private static void SetTextAndColor(WorkStateLabel label, WorkState state)
-        {
-            label.Text = state switch
-            {
-                WorkState.Pending => "Pending",
-                WorkState.Working => "Working",
-                WorkState.Done => "Done",
-                _ => string.Empty
-            };
-
-            Color? color = state switch
-            {
-                WorkState.Pending => label.PendingColor,
-                WorkState.Working => label.WorkingColor,
-                WorkState.Done => label.DoneColor,
-                _ => null
-            };
-
-            if (color is not null)
-            {
-                label.TextColor = color;
-            }
-        }
-
-        public WorkState Source
+        public WorkState? Source
         {
             get => (WorkState)GetValue(SourceProperty);
             set { SetValue(SourceProperty, value); }
         }
 
-        public Color PendingColor
+        public Color? PendingColor
         {
             get => (Color)GetValue(PendingColorProperty);
             set { SetValue(PendingColorProperty, value); }
         }
 
-        public Color WorkingColor
+        public Color? WorkingColor
         {
             get => (Color)GetValue(WorkingColorProperty);
             set { SetValue(WorkingColorProperty, value); }
         }
 
-        public Color DoneColor
+        public Color? DoneColor
         {
             get => (Color)GetValue(DoneColorProperty);
             set { SetValue(DoneColorProperty, value); }
@@ -83,7 +56,35 @@ namespace Fasciculus.Maui.Controls
 
         public WorkStateLabel()
         {
-            SetTextAndColor(this, WorkState.Pending);
+            SetTextAndColor();
+        }
+
+        private void SetTextAndColor()
+        {
+            SetText();
+            SetTextColor();
+        }
+
+        private void SetText()
+        {
+            Text = Source switch
+            {
+                WorkState.Pending => "Pending",
+                WorkState.Working => "Working",
+                WorkState.Done => "Done",
+                _ => string.Empty
+            };
+        }
+
+        private void SetTextColor()
+        {
+            TextColor = Source switch
+            {
+                WorkState.Pending => PendingColor ?? Colors.Red,
+                WorkState.Working => WorkingColor ?? Colors.Yellow,
+                WorkState.Done => DoneColor ?? Colors.Green,
+                _ => Colors.Transparent
+            };
         }
     }
 }
