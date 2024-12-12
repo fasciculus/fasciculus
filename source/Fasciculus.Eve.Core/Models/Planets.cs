ab4e4eb35a2102415e2fd613275ac5180e6492ab
rhjoerg <rhjoerg@gmail.com>
2024-12-12 17:19:43 +0100

    added EvePlanetBaseCosts

--- source/Fasciculus.Eve.Core/Models/Planets.cs
+++ source/Fasciculus.Eve.Core/Models/Planets.cs
@@ -227,4 +227,22 @@ namespace Fasciculus.Eve.Models
             return [.. planetSchematics.Where(filter).OrderBy(x => x.Id)];
         }
     }
+
+    public class EvePlanetBaseCosts
+    {
+        private readonly Dictionary<EveType, double> costs;
+
+        public double this[EveType type]
+            => costs.TryGetValue(type, out double result) ? result : 0.0;
+
+        public EvePlanetBaseCosts(EvePlanetSchematics schematics)
+        {
+            costs = schematics.P0.Select(x => Tuple.Create(x, 5.0))
+                .Concat(schematics.P1.Select(x => Tuple.Create(x.OutputType, 400.0)))
+                .Concat(schematics.P2.Select(x => Tuple.Create(x.OutputType, 7_200.0)))
+                .Concat(schematics.P3.Select(x => Tuple.Create(x.OutputType, 60_000.0)))
+                .Concat(schematics.P4.Select(x => Tuple.Create(x.OutputType, 1_200_000.0)))
+                .ToDictionary();
+        }
+    }
 }
