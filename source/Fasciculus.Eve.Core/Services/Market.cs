10e9c9f212e8273252a0dd0fbf6d1a5d18a2ad84
rhjoerg <rhjoerg@gmail.com>
2024-12-12 17:57:18 +0100

    added Market.Hub

--- source/Fasciculus.Eve.Core/Services/Market.cs
+++ source/Fasciculus.Eve.Core/Services/Market.cs
@@ -1,11 +1,20 @@
-﻿namespace Fasciculus.Eve.Services
+﻿using Fasciculus.Eve.Models;
+using Fasciculus.Threading;
+
+namespace Fasciculus.Eve.Services
 {
     public interface IMarket
     {
-
+        public EveStation Hub { get; }
     }
 
     public class Market : IMarket
     {
+        public EveStation Hub { get; }
+
+        public Market(IEveResources resources)
+        {
+            Hub = Tasks.Wait(resources.Universe).Stations[60003760];
+        }
     }
 }
