137d6beb6de812e170728d06f6ffa6eb583d4abf
rhjoerg <rhjoerg@gmail.com>
2024-12-13 15:11:39 +0100

    adding Isklabel

---
+++ source/Fasciculus.Eve/Controls/IskLabel.cs
@@ -0,0 +1,33 @@
+﻿using Fasciculus.Eve.Support;
+
+namespace Fasciculus.Eve.Controls
+{
+    public partial class IskLabel : Label
+    {
+        public static readonly BindableProperty ValueProperty
+            = BindableProperty.Create(nameof(Value), typeof(double), typeof(IskLabel), null,
+                BindingMode.OneWay, null, OnValuePropertyChanged);
+
+        public double Value
+        {
+            get => (double)GetValue(ValueProperty);
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public IskLabel()
+        {
+            HorizontalTextAlignment = TextAlignment.End;
+        }
+
+        private static void OnValuePropertyChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            if (bindable is IskLabel label)
+            {
+                if (newvalue is double value)
+                {
+                    label.Text = value.ToString("#,###,###,##0", EveFormats.Isk);
+                }
+            }
+        }
+    }
+}
---
+++ source/Fasciculus.Eve/Support/EveFormats.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+
+namespace Fasciculus.Eve.Support
+{
+    public static class EveFormats
+    {
+        public static readonly NumberFormatInfo Isk = CreateIsk();
+
+        private static NumberFormatInfo CreateIsk()
+        {
+            CultureInfo ci = CultureInfo.GetCultureInfo("en-US");
+            NumberFormatInfo nf = (NumberFormatInfo)ci.NumberFormat.Clone();
+
+            nf.NumberGroupSeparator = "'";
+
+            return nf;
+        }
+    }
+}
