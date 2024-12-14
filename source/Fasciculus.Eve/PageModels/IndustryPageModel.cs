c58400062dd82ad9d844131d38cbda5bdae9c1be
rhjoerg <rhjoerg@gmail.com>
2024-12-14 08:56:05 +0100

    more industry

--- source/Fasciculus.Eve/PageModels/IndustryPageModel.cs
+++ source/Fasciculus.Eve/PageModels/IndustryPageModel.cs
@@ -1,11 +1,21 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using Fasciculus.Eve.Services;
+using Fasciculus.Threading;
 
 namespace Fasciculus.Eve.PageModels
 {
     public partial class IndustryPageModel : ObservableObject
     {
-        public IndustryPageModel()
+        [ObservableProperty]
+        private List<string> solarSystems;
+
+        [ObservableProperty]
+        private string selectedSolarSystem;
+
+        public IndustryPageModel(IEveResources resources)
         {
+            solarSystems = [.. Tasks.Wait(resources.Universe).SolarSystems.Select(x => x.Name).OrderBy(x => x)];
+            selectedSolarSystem = "Jita";
         }
     }
 }
