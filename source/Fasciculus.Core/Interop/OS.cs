985fbe949f044c3dbd9ebf01238dad8b3b86e6b9
rhjoerg <rhjoerg@gmail.com>
2024-12-25 07:58:08 +0100

    documented OS

--- source/Fasciculus.Core/Interop/OS.cs
+++ source/Fasciculus.Core/Interop/OS.cs
@@ -2,8 +2,17 @@
 
 namespace Fasciculus.Interop
 {
+    /// <summary>
+    /// OS information.
+    /// </summary>
     public static class OS
     {
+        /// <summary>
+        /// Whether the corrent OS is windows.
+        /// <para>
+        /// Shorthand for <c>RuntimeInformation.IsOSPlatform(OSPlatform.Windows)</c>.
+        /// </para>
+        /// </summary>
         public static bool IsWindows
         {
             get => RuntimeInformation.IsOSPlatform(OSPlatform.Windows);
