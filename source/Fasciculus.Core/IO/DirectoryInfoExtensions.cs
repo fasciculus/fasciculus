68d0cecb1f465fb4134075884ca000455bd78609
rhjoerg <rhjoerg@gmail.com>
2024-12-25 13:11:34 +0100

    documented DirectoryInfoExtensions

--- source/Fasciculus.Core/IO/DirectoryInfoExtensions.cs
+++ source/Fasciculus.Core/IO/DirectoryInfoExtensions.cs
@@ -1,10 +1,20 @@
 ﻿namespace System.IO
 {
+    /// <summary>
+    /// Extensions for <see cref="DirectoryInfo"/>.
+    /// </summary>
     public static class DirectoryInfoExtensions
     {
+        /// <summary>
+        /// Adds the given <paramref name="name"/> to the given <paramref name="directory"/> and returns the resulting <see cref="FileInfo"/>.
+        /// </summary>
         public static FileInfo File(this DirectoryInfo directory, string name)
             => new(Path.Combine(directory.FullName, name));
 
+        /// <summary>
+        /// Creates the given <paramref name="directory"/> if it doesn't exist yet.
+        /// </summary>
+        /// <returns></returns>
         public static DirectoryInfo CreateIfNotExists(this DirectoryInfo directory)
         {
             DirectoryInfo result = new(directory.FullName);
@@ -18,6 +28,10 @@
             return result;
         }
 
+        /// <summary>
+        /// Adds the path elements given in <paramref name="paths"/> to the given <paramref name="directory"/> and returns the resulting
+        /// <see cref="DirectoryInfo"/>.
+        /// </summary>
         public static DirectoryInfo Combine(this DirectoryInfo directory, params string[] paths)
         {
             string result = directory.FullName;
