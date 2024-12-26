674d04da15e5a2dcf0d5dd47e193983a5c506552
rhjoerg <rhjoerg@gmail.com>
2024-12-26 10:38:05 +0100

    documented Cond and Ex

--- source/Fasciculus.Core/Support/Cond.cs
+++ source/Fasciculus.Core/Support/Cond.cs
@@ -2,14 +2,23 @@
 
 namespace Fasciculus.Support
 {
+    /// <summary>
+    /// Support for conditions.
+    /// </summary>
     public static class Cond
     {
+        /// <summary>
+        /// Throws an <see cref="InvalidOperationException"/> if the given <paramref name="value"/> is <c>null</c>.
+        /// </summary>
         public static T NotNull<T>(T? value)
             where T : notnull
         {
             return value ?? throw new InvalidOperationException();
         }
 
+        /// <summary>
+        /// Throws an <see cref="InvalidOperationException"/> if the given <paramref name="value"/> is not <c>null</c>.
+        /// </summary>
         public static void IsNull<T>(T? value)
             where T : notnull
         {
--- source/Fasciculus.Core/Support/Ex.cs
+++ source/Fasciculus.Core/Support/Ex.cs
@@ -4,23 +4,41 @@ using System.IO;
 
 namespace Fasciculus.Support
 {
+    /// <summary>
+    /// Exception helper.
+    /// </summary>
     public static class Ex
     {
         private static string Format(string? format, params object[] args)
             => format is null ? string.Empty : string.Format(format, args);
 
+        /// <summary>
+        /// Creates a new <see cref="NotImplementedException"/>.
+        /// </summary>
         public static NotImplementedException NotImplemented()
             => new();
 
+        /// <summary>
+        /// Creates a new <see cref="IndexOutOfRangeException"/>.
+        /// </summary>
         public static IndexOutOfRangeException IndexOutOfRange()
             => new();
 
+        /// <summary>
+        /// Creates a new <see cref="FileNotFoundException"/>.
+        /// </summary>
         public static FileNotFoundException FileNotFound(FileInfo? fileInfo = null)
             => new(Format(fileInfo?.FullName));
 
+        /// <summary>
+        /// Creates a new <see cref="EndOfStreamException"/>.
+        /// </summary>
         public static EndOfStreamException EndOfStream()
             => new();
 
+        /// <summary>
+        /// Creates a new <see cref="ResourceNotFoundException"/>.
+        /// </summary>
         public static ResourceNotFoundException ResourceNotFound(string? name = null)
             => new(Format(name));
     }
