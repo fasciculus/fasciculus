979a67ba2e3b0e830b1ff65f4b3251af7fc17763
rhjoerg <rhjoerg@gmail.com>
2024-12-26 10:21:31 +0100

    documented AssemblyExtensions

--- source/Fasciculus.Core/Reflection/AssemblyExtensions.cs
+++ source/Fasciculus.Core/Reflection/AssemblyExtensions.cs
@@ -3,8 +3,18 @@ using System.Linq;
 
 namespace System.Reflection
 {
+    /// <summary>
+    /// <see cref="Assembly"/> extensions.
+    /// </summary>
     public static class AssemblyExtensions
     {
+        /// <summary>
+        /// Returns the custom attribute of type <typeparamref name="T"/> of the given <paramref name="assembly"/>.
+        /// </summary>
+        /// <returns>
+        /// The custom attribute or <c>null</c> if <paramref name="assembly"/> is <c>null</c> or no such attribute exists
+        /// in the assembly.
+        /// </returns>
         public static T? GetAttribute<T>(this Assembly? assembly)
             where T : Attribute
         {
@@ -22,18 +32,27 @@ namespace System.Reflection
             return attribute is not null && attribute is T result ? result : null;
         }
 
+        /// <summary>
+        /// Returns the value of the <see cref="AssemblyInformationalVersionAttribute"/> attribute of the given <paramref name="assembly"/>.
+        /// <para>
+        /// Resorts to <see cref="GetFileVersion(Assembly?)"/> if no such attribute exists.
+        /// </para>
+        /// </summary>
+        public static string GetSemanticVersion(this Assembly? assembly)
+        {
+            return assembly?.GetAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
+                ?? assembly?.GetFileVersion() ?? string.Empty;
+        }
+
+        /// <summary>
+        /// Returns the file version of the given <paramref name="assembly"/>.
+        /// </summary>
         public static string GetFileVersion(this Assembly? assembly)
         {
             AssemblyName? name = assembly?.GetName();
             Version? version = name?.Version;
 
             return version?.ToString() ?? string.Empty;
-        }
-
-        public static string GetSemanticVersion(this Assembly? assembly)
-        {
-            return assembly?.GetAttribute<AssemblyInformationalVersionAttribute>()?.InformationalVersion
-                ?? assembly?.GetFileVersion() ?? string.Empty;
         }
     }
 }
