6d3ef84df8cb450cbbc702178347627d89dbe2dc
rhjoerg <rhjoerg@gmail.com>
2024-12-27 04:34:10 +0100

    added MainThreadSortedSet

---
+++ source/Fasciculus.Maui/Collections/MainThreadSortedSet.cs
@@ -0,0 +1,48 @@
+﻿using Fasciculus.Collections;
+using Fasciculus.Maui.Threading;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace Fasciculus.Maui.Collections
+{
+    /// <summary>
+    /// An observable sorted set propagating events on the main thread
+    /// </summary>
+    public class MainThreadSortedSet<T> : ObservableSortedSet<T>
+    {
+        /// <summary>
+        /// Initializes a set with the given <paramref name="collection"/> and the given <paramref name="comparer"/>.
+        /// </summary>
+        public MainThreadSortedSet(IEnumerable<T> collection, IComparer<T> comparer)
+            : base(collection, comparer) { }
+
+        /// <summary>
+        /// Initializes a set with the given <paramref name="collection"/>
+        /// </summary>
+        public MainThreadSortedSet(IEnumerable<T> collection)
+            : base(collection) { }
+
+        /// <summary>
+        /// Initializes a set with the given <paramref name="comparer"/>.
+        /// </summary>
+        public MainThreadSortedSet(IComparer<T> comparer)
+            : base(comparer) { }
+
+        /// <summary>
+        /// Initializes an empty set.
+        /// </summary>
+        public MainThreadSortedSet() { }
+
+        /// <summary>
+        /// Called when a property of this set changed.
+        /// </summary>
+        protected override void OnPropertyChanged(string name)
+            => Threads.RunInMainThread(() => { base.OnPropertyChanged(name); });
+
+        /// <summary>
+        /// Called when the contents of this set changed.
+        /// </summary>
+        protected override void OnCollectionChanged(NotifyCollectionChangedAction action, T item)
+            => Threads.RunInMainThread(() => { base.OnCollectionChanged(action, item); });
+    }
+}
