c3a9f0db20003d2111e4b3b65edf9d4bedea9974
rhjoerg <rhjoerg@gmail.com>
2024-12-27 04:26:31 +0100

    added TaskSafeSortedSet and ObservableSortedSet

---
+++ source/Fasciculus.Core/Collections/ObservableSortedSet.cs
@@ -0,0 +1,207 @@
+﻿using Fasciculus.Threading.Synchronization;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Fasciculus.Collections
+{
+    /// <summary>
+    /// Observable and task-safe sorted set.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ObservableSortedSet<T> : TaskSafeSortedSet<T>, INotifyPropertyChanged, INotifyCollectionChanged
+    {
+        private readonly TaskSafeMutex mutex = new();
+
+        /// <summary>
+        /// Notifies clients that a property value has changed.
+        /// </summary>
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        /// <summary>
+        /// Notifies clients that a property value has changed.
+        /// </summary>
+        public event NotifyCollectionChangedEventHandler? CollectionChanged;
+
+        /// <summary>
+        /// Initializes a set with the given <paramref name="collection"/> and the given <paramref name="comparer"/>.
+        /// </summary>
+        public ObservableSortedSet(IEnumerable<T> collection, IComparer<T> comparer)
+            : base(collection, comparer) { }
+
+        /// <summary>
+        /// Initializes a set with the given <paramref name="collection"/>
+        /// </summary>
+        public ObservableSortedSet(IEnumerable<T> collection)
+            : base(collection) { }
+
+        /// <summary>
+        /// Initializes a set with the given <paramref name="comparer"/>.
+        /// </summary>
+        public ObservableSortedSet(IComparer<T> comparer)
+            : base(comparer) { }
+
+        /// <summary>
+        /// Initializes an empty set.
+        /// </summary>
+        public ObservableSortedSet() { }
+
+        /// <summary>
+        /// Adds an element to the current set and returns a value to indicate if the element was successfully added.
+        /// </summary>
+        public override bool Add(T item)
+        {
+            using Locker locker = Locker.Lock(mutex);
+            bool result = base.Add(item);
+
+            if (result)
+            {
+                OnAdded(item);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Removes an element from the current set and returns a value to indicate if the element was successfully removed.
+        /// </summary>
+        public override bool Remove(T item)
+        {
+            using Locker locker = Locker.Lock(mutex);
+            bool result = base.Remove(item);
+
+            if (result)
+            {
+                OnRemoved(item);
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Removes all elements from the set.
+        /// </summary>
+        public override void Clear()
+        {
+            using Locker locker = Locker.Lock(mutex);
+
+            while (Count > 0)
+            {
+                T item = this.First();
+
+                base.Remove(item);
+                OnRemoved(item);
+            }
+        }
+
+        /// <summary>
+        /// Modifies the set so that it contains all elements that are present in either the set or the <paramref name="other"/> collection.
+        /// </summary>
+        public override void UnionWith(IEnumerable<T> other)
+        {
+            using Locker locker = Locker.Lock(mutex);
+
+            foreach (T item in other)
+            {
+                if (base.Add(item))
+                {
+                    OnAdded(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Modifies the set so that it contains all elements that are present in both the set and the <paramref name="other"/> collection.
+        /// </summary>
+        public override void IntersectWith(IEnumerable<T> other)
+        {
+            using Locker locker = Locker.Lock(mutex);
+            T[] items = [.. this];
+
+            foreach (T item in items)
+            {
+                if (!other.Contains(item))
+                {
+                    base.Remove(item);
+                    OnRemoved(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes all elements in <paramref name="other"/> from this set.
+        /// </summary>
+        public override void ExceptWith(IEnumerable<T> other)
+        {
+            using Locker locker = Locker.Lock(mutex);
+
+            foreach (T item in other)
+            {
+                if (base.Remove(item))
+                {
+                    OnRemoved(item);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Modifies the set so that it contains all elements that are present either in this set or in the <paramref name="other"/> collection
+        /// but not in both.
+        /// </summary>
+        public override void SymmetricExceptWith(IEnumerable<T> other)
+        {
+            using Locker locker = Locker.Lock(mutex);
+            SortedSet<T> intersection = new(this, Comparer);
+
+            intersection.IntersectWith(other);
+
+            foreach (T item in intersection)
+            {
+                base.Remove(item);
+                OnRemoved(item);
+            }
+
+            foreach (T item in other)
+            {
+                if (!intersection.Contains(item))
+                {
+                    base.Add(item);
+                    OnAdded(item);
+                }
+            }
+        }
+
+        private void OnAdded(T item)
+        {
+            OnPropertyChanged(nameof(Count));
+            OnCollectionChanged(NotifyCollectionChangedAction.Add, item);
+        }
+
+        private void OnRemoved(T item)
+        {
+            OnPropertyChanged(nameof(Count));
+            OnCollectionChanged(NotifyCollectionChangedAction.Remove, item);
+        }
+
+        /// <summary>
+        /// Called when a property of this set changed.
+        /// </summary>
+        protected virtual void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventArgs args = new(name);
+
+            PropertyChanged?.Invoke(this, args);
+        }
+
+        /// <summary>
+        /// Called when the contents of this set changed.
+        /// </summary>
+        protected virtual void OnCollectionChanged(NotifyCollectionChangedAction action, T item)
+        {
+            NotifyCollectionChangedEventArgs args = new(action, item);
+
+            CollectionChanged?.Invoke(this, args);
+        }
+    }
+}
---
+++ source/Fasciculus.Core/Collections/TaskSafeSortedSet.cs
@@ -0,0 +1,200 @@
+﻿using Fasciculus.Threading.Synchronization;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Fasciculus.Collections
+{
+    /// <summary>
+    /// A task safe sorted set.
+    /// </summary>
+    public class TaskSafeSortedSet<T> : ISet<T>, ICollection<T>, IReadOnlyCollection<T>, ICollection, IEnumerable<T>, IEnumerable
+    {
+        private readonly TaskSafeMutex mutex = new();
+        private readonly SortedSet<T> values;
+
+        /// <summary>
+        /// The number of elements in the set.
+        /// </summary>
+        public int Count => Locker.Locked(mutex, () => values.Count);
+
+        bool ICollection<T>.IsReadOnly => false;
+        bool ICollection.IsSynchronized => false;
+        object ICollection.SyncRoot => this;
+
+        /// <summary>
+        /// The comparer used to sort the set.
+        /// </summary>
+        public IComparer<T> Comparer => values.Comparer;
+
+        /// <summary>
+        /// Initializes a set with the given <paramref name="collection"/> and the given <paramref name="comparer"/>.
+        /// </summary>
+        public TaskSafeSortedSet(IEnumerable<T> collection, IComparer<T> comparer)
+        {
+            values = new(collection, comparer);
+        }
+
+        /// <summary>
+        /// Initializes a set with the given <paramref name="collection"/>
+        /// </summary>
+        public TaskSafeSortedSet(IEnumerable<T> collection)
+            : this(collection, Comparer<T>.Default) { }
+
+        /// <summary>
+        /// Initializes a set with the given <paramref name="comparer"/>.
+        /// </summary>
+        public TaskSafeSortedSet(IComparer<T> comparer)
+            : this([], comparer) { }
+
+        /// <summary>
+        /// Initializes an empty set.
+        /// </summary>
+        public TaskSafeSortedSet()
+            : this([], Comparer<T>.Default) { }
+
+        /// <summary>
+        /// Adds an element to the current set and returns a value to indicate if the element was successfully added.
+        /// </summary>
+        public virtual bool Add(T item)
+            => Locker.Locked(mutex, () => values.Add(item));
+
+        void ICollection<T>.Add(T item)
+            => Add(item);
+
+        /// <summary>
+        /// Removes an element from the current set and returns a value to indicate if the element was successfully removed.
+        /// </summary>
+        public virtual bool Remove(T item)
+            => Locker.Locked(mutex, () => values.Remove(item));
+
+        /// <summary>
+        /// Removes all elements from the set.
+        /// </summary>
+        public virtual void Clear()
+            => Locker.Locked(mutex, () => values.Clear());
+
+        /// <summary>
+        /// Determines whether the set contains a specific element.
+        /// </summary>
+        public bool Contains(T item)
+             => Locker.Locked(mutex, () => values.Contains(item));
+
+        /// <summary>
+        /// Copies the set into the given <paramref name="array"/>.
+        /// <para>
+        /// Note: the <paramref name="array"/> must be large enough to hold <see cref="Count"/> values.
+        /// </para>
+        /// </summary>
+        public void CopyTo(T[] array)
+            => Locker.Locked(mutex, () => values.CopyTo(array));
+
+        /// <summary>
+        /// Copies the set into the given <paramref name="array"/> starting at array position <paramref name="index"/>.
+        /// <para>
+        /// Note: the <paramref name="array"/> must be large enough to hold <paramref name="index"/> + <see cref="Count"/> values.
+        /// </para>
+        /// </summary>
+        public void CopyTo(T[] array, int index)
+            => Locker.Locked(mutex, () => values.CopyTo(array, index));
+
+        /// <summary>
+        /// Copies <paramref name="count"/> values from the set into the given <paramref name="array"/>
+        /// starting at array position <paramref name="index"/>.
+        /// <para>
+        /// Note: the <paramref name="array"/> must be large enough to hold <paramref name="index"/> + <paramref name="count"/> values.
+        /// </para>
+        /// </summary>
+        public void CopyTo(T[] array, int index, int count)
+            => Locker.Locked(mutex, () => values.CopyTo(array, index, count));
+
+        void ICollection.CopyTo(Array array, int index)
+            => Locker.Locked(mutex, () => (values as ICollection).CopyTo(array, index));
+
+        /// <summary>
+        /// Modifies the set so that it contains all elements that are present in either the set or the <paramref name="other"/> collection.
+        /// </summary>
+        public virtual void UnionWith(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.UnionWith(other));
+
+        /// <summary>
+        /// Modifies the set so that it contains all elements that are present in both the set and the <paramref name="other"/> collection.
+        /// </summary>
+        public virtual void IntersectWith(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.IntersectWith(other));
+
+        /// <summary>
+        /// Removes all elements in <paramref name="other"/> from this set.
+        /// </summary>
+        public virtual void ExceptWith(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.ExceptWith(other));
+
+        /// <summary>
+        /// Modifies the set so that it contains all elements that are present either in this set or in the <paramref name="other"/> collection
+        /// but not in both.
+        /// </summary>
+        public virtual void SymmetricExceptWith(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.SymmetricExceptWith(other));
+
+        /// <summary>
+        /// Determines whether this set is a subset of the <paramref name="other"/> collection.
+        /// </summary>
+        public bool IsSubsetOf(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.IsSubsetOf(other));
+
+        /// <summary>
+        /// Determines whether this set is a proper subset of the <paramref name="other"/> collection.
+        /// </summary>
+        public bool IsProperSubsetOf(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.IsProperSubsetOf(other));
+
+        /// <summary>
+        /// Determines whether this set is a superset of the <paramref name="other"/> collection.
+        /// </summary>
+        public bool IsSupersetOf(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.IsSupersetOf(other));
+
+        /// <summary>
+        /// Determines whether this set is a proper superset of the <paramref name="other"/> collection.
+        /// </summary>
+        public bool IsProperSupersetOf(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.IsProperSupersetOf(other));
+
+        /// <summary>
+        /// Determines whether this set and the <paramref name="other"/> collection contain the same elements.
+        /// </summary>
+        public bool SetEquals(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.SetEquals(other));
+
+        /// <summary>
+        /// Determines whether this set and the <paramref name="other"/> collection share common elements.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        public bool Overlaps(IEnumerable<T> other)
+            => Locker.Locked(mutex, () => values.Overlaps(other));
+
+        private T[] GetArray()
+            => Locker.Locked(mutex, () => values.ToArray());
+
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// </summary>
+        public IEnumerator<T> GetEnumerator()
+            => GetArray().AsEnumerable().GetEnumerator();
+
+        IEnumerator IEnumerable.GetEnumerator()
+            => GetArray().GetEnumerator();
+
+        /// <summary>
+        /// Returns the index of the given <paramref name="item"/> in the set.
+        /// </summary>
+        public int IndexOf(T item)
+        {
+            T[] array = GetArray();
+
+            return Array.BinarySearch(array, 0, array.Length, item, Comparer);
+        }
+    }
+}
