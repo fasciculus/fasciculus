ef5ae876a05c9f8bd14a033e3f351dedcd8d8150
rhjoerg <rhjoerg@gmail.com>
2024-12-29 10:09:28 +0100

    added Fasciculus.FileServer

---
+++ source/Fasciculus.FileServer/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Fasciculus.FileServer
+{
+    public class Program
+    {
+        private static readonly FileServerOptions fileServerOptions = new()
+        {
+            EnableDirectoryBrowsing = true,
+            StaticFileOptions =
+            {
+                ServeUnknownFileTypes = true,
+                DefaultContentType = "application/octet-stream"
+            }
+        };
+
+        public static void Main(string[] args)
+        {
+            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDirectoryBrowser();
+
+            WebApplication app = builder.Build();
+
+            app.Use(TryHtml);
+            app.UseFileServer(fileServerOptions);
+            app.Run();
+        }
+
+        private static async Task TryHtml(HttpContext context, Func<Task> next)
+        {
+            await next.Invoke();
+
+            if (context.Response.StatusCode != 404)
+            {
+                return;
+            }
+
+            context.Request.Path += ".html";
+
+            await next.Invoke();
+        }
+    }
+}
