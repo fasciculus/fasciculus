215e2d35da5427305fffc2d5c8cce28f1e354a80
rhjoerg <rhjoerg@gmail.com>
2025-01-01 12:58:27 +0100

    Added wwwroot

--- source/Fasciculus.GitHub.Io/Services/Documents.cs
+++ source/Fasciculus.GitHub.Io/Services/Documents.cs
@@ -1,4 +1,6 @@
 ﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace Fasciculus.GitHub.Services
 {
@@ -7,12 +9,23 @@ namespace Fasciculus.GitHub.Services
         public Documents()
         {
             AddGlobals();
+            AddStatics();
         }
 
         private void AddGlobals()
         {
             Add("/");
             Add("/Privacy");
+        }
+
+        private void AddStatics()
+        {
+            DirectoryInfo wwwroot = new(Path.GetFullPath("wwwroot"));
+            FileInfo[] files = [.. wwwroot.EnumerateFiles("*", SearchOption.AllDirectories)];
+            string[] relative = [.. files.Select(file => Path.GetRelativePath(wwwroot.FullName, file.FullName))];
+            string[] paths = [.. relative.Select(path => "/" + path.Replace("\\", "/"))];
+
+            paths.Apply(Add);
         }
     }
 }
