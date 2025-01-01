b84e02d038ac3824ff50d67847bb0b4896ce3de5
rhjoerg <rhjoerg@gmail.com>
2025-01-01 17:13:25 +0100

    experimenting with roslyn

--- source/Fasciculus.Experiments.ApiDoc/Program.cs
+++ source/Fasciculus.Experiments.ApiDoc/Program.cs
@@ -1,9 +1,23 @@
-﻿namespace Fasciculus.Experiments.ApiDoc
+﻿using Fasciculus.IO.Searching;
+using Microsoft.CodeAnalysis;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace Fasciculus.Experiments.ApiDoc
 {
     public static class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            FileInfo solutionFile = FileSearch.Search("fasciculus.sln", SearchPath.WorkingDirectoryAndParents).First();
+            string[] solutionLines = [.. solutionFile.ReadAllLines().Select(x => x.Trim())];
+            string solutionGuidLine = solutionLines.First(x => x.StartsWith("SolutionGuid"));
+            Guid solutionGuid = Guid.Parse(solutionGuidLine.AsSpan("SolutionGuid = ".Length));
+            SolutionId solutionId = SolutionId.CreateFromSerialized(solutionGuid);
+            SolutionInfo solutionInfo = SolutionInfo.Create(solutionId, VersionStamp.Default, solutionFile.FullName);
+            AdhocWorkspace workspace = new();
+            Solution solution = workspace.AddSolution(solutionInfo);
         }
     }
 }
