ed82a2b45106e60a0cd49b269eeb462e5ade02af
rhjoerg <rhjoerg@gmail.com>
2025-01-08 08:42:36 +0100

    added SymbolComment

---
+++ source/Fasciculus.CodeAnalysis/Commenting/SymbolComment.cs
@@ -0,0 +1,41 @@
+﻿using System.Xml;
+
+namespace Fasciculus.CodeAnalysis.Commenting
+{
+    public class SymbolComment
+    {
+        public XmlDocument Document { get; }
+
+        private XmlElement? summary;
+
+        public bool HasSummary => summary is not null;
+
+        public string Summary => summary?.InnerXml ?? string.Empty;
+
+        public SymbolComment(XmlDocument document)
+        {
+            Document = document;
+
+            summary = FindElement(document, "summary");
+        }
+
+        private static XmlElement? FindElement(XmlDocument document, string name)
+        {
+            XmlNodeList nodes = document.GetElementsByTagName(name);
+
+            if (nodes.Count > 0)
+            {
+                XmlNode? node = nodes[0];
+
+                if (node is not null && node is XmlElement element)
+                {
+                    return element;
+                }
+            }
+
+            return null;
+        }
+
+        public static readonly SymbolComment Empty = SymbolCommentFactory.CreateEmpty();
+    }
+}
---
+++ source/Fasciculus.CodeAnalysis/Commenting/SymbolCommentFactory.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using System.Xml;
+
+namespace Fasciculus.CodeAnalysis.Commenting
+{
+    public class SymbolCommentFactory
+    {
+        public SymbolComment Create(string xml)
+        {
+            try
+            {
+                XmlDocument document = new();
+
+                document.LoadXml(xml);
+
+                return new(document);
+            }
+            catch
+            {
+                Debugger.Break();
+
+                return SymbolComment.Empty;
+            }
+        }
+
+        public static SymbolComment CreateEmpty()
+        {
+            SymbolCommentFactory factory = new();
+
+            return factory.Create("<comment />");
+        }
+    }
+}
