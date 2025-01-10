9b9186ec2671a139a2cb2e604c71ccc4257fd75e
rhjoerg <rhjoerg@gmail.com>
2025-01-10 09:06:41 +0100

    cleanup

--- source/Fasciculus.ApiDoc/Models/ApiDocuments.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Fasciculus.ApiDoc.Models
-{
-    public class ApiDocuments
-    {
-        public required ApiPackages Packages { get; init; }
-        public required ApiPackage Combined { get; init; }
-    }
-}
