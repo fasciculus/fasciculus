4fc7ff389a7bce35c9c1f058b2ddd8e1d2795da6
rhjoerg <rhjoerg@gmail.com>
2025-01-16 10:10:33 +0100

    more syntax highlighting

--- source/Fasciculus.Experiments.SyntaxHighlighting/Program.cs
+++ source/Fasciculus.Experiments.SyntaxHighlighting/Program.cs
@@ -3,10 +3,8 @@ using Fasciculus.IO;
 using Fasciculus.IO.Resources;
 using Markdig;
 using Markdig.Syntax;
-using Microsoft.ClearScript.V8;
 using System;
 using System.Diagnostics;
-using System.IO;
 using System.Text;
 
 namespace Fasciculus.Experiments.SyntaxHighlighting
@@ -24,25 +22,11 @@ namespace Fasciculus.Experiments.SyntaxHighlighting
 
         private static void ProcessDocument(string documentName)
         {
-            string markdown = EmbeddedResources.Find(documentName).Read(ReadDocument, false);
+            string markdown = EmbeddedResources.Find(documentName).ReadString(false, Encoding.UTF8);
 
             MarkdownPipeline markdownPipeline = new MarkdownPipelineBuilder().Build();
             MarkdownDocument markdownDocument = Markdown.Parse(markdown, markdownPipeline);
             string html = markdownDocument.ToHtml(markdownPipeline);
-
-            using V8ScriptEngine engine = new();
-
-            engine.AddHostType("Console", typeof(Console));
-            engine.Execute("Console.WriteLine('{0} is an interesting number.', Math.PI)");
-        }
-
-        private static string ReadDocument(Stream stream)
-        {
-            byte[] buffer = new byte[stream.Length];
-
-            stream.ReadExactly(buffer);
-
-            return Encoding.UTF8.GetString(buffer);
         }
 
         private static void Log(object? obj)
