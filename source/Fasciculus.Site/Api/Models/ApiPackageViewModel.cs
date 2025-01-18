61b6b1b1e16d418076b6c416dac7374713d180c8
rhjoerg <rhjoerg@gmail.com>
2025-01-18 06:46:01 +0100

    added external link to package vew

--- source/Fasciculus.Site/Api/Models/ApiPackageViewModel.cs
+++ source/Fasciculus.Site/Api/Models/ApiPackageViewModel.cs
@@ -1,9 +1,8 @@
 using Fasciculus.CodeAnalysis.Models;
-using Fasciculus.Site.Models;
 
 namespace Fasciculus.Site.Api.Models
 {
-    public class ApiPackageViewModel : ViewModel
+    public class ApiPackageViewModel : ApiViewModel
     {
         public required PackageSymbol Package { get; init; }
     }
