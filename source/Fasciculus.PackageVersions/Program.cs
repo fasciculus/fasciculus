e902bd1e97dcf8ced3a6fe745ec5e810cb32795b
rhjoerg <rhjoerg@gmail.com>
2025-01-24 06:13:31 +0100

    simplified Fasciculus.PackageVersions

--- source/Fasciculus.PackageVersions/Program.cs
+++ source/Fasciculus.PackageVersions/Program.cs
@@ -1,6 +1,11 @@
-using Fasciculus.PackageVersions.Models;
-using Fasciculus.PackageVersions.Services;
+using Fasciculus.NuGet.Logging;
+using Fasciculus.NuGet.Services;
+using NuGet.Common;
+using NuGet.Packaging.Core;
+using NuGet.Protocol.Core.Types;
+using NuGet.Versioning;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Fasciculus.PackageVersions
 {
@@ -8,11 +13,26 @@ namespace Fasciculus.PackageVersions
     {
         public static void Main(string[] args)
         {
-            using VersionsProvider versionsProvider = new();
-            VersionsChecker versionsChecker = new(versionsProvider);
-            List<PackageInfo> packages = PackagesProvider.GetPackages();
+            DirectoryPackages packagesService = new();
+            using SourceCacheContext cache = new();
+            NuGetResources resources = new();
+            ConsoleLogger logger = new(LogLevel.Warning);
+            NuGetVersions versionsService = new(resources, cache, logger);
 
-            versionsChecker.Check(packages);
+            PackageIdentity[] packages = packagesService.GetPackages();
+
+            foreach (PackageIdentity package in packages)
+            {
+                bool includePrerelease = package.Version.IsPrerelease;
+                SortedSet<NuGetVersion> versions = versionsService.GetVersions(package.Id, includePrerelease);
+
+                if (versions.Any(v => v > package.Version))
+                {
+                    string message = $"{package.Id}: {package.Version} -> {versions.Last()}";
+
+                    logger.LogWarning(message);
+                }
+            }
         }
     }
 }
