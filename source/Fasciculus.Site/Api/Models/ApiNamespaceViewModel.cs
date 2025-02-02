25a9f9e85403b92b246e32940d41b29b52a57af0
rhjoerg <rhjoerg@gmail.com>
2025-02-02 03:49:34 +0100

    simplified ApiNamespaceViewModel

--- source/Fasciculus.Site/Api/Models/ApiNamespaceViewModel.cs
+++ source/Fasciculus.Site/Api/Models/ApiNamespaceViewModel.cs
@@ -1,22 +1,10 @@
 using Fasciculus.CodeAnalysis.Models;
 using Fasciculus.Site.Models;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace Fasciculus.Site.Api.Models
 {
     public class ApiNamespaceViewModel : ViewModel
     {
         public required INamespaceSymbol Namespace { get; init; }
-
-        public IEnumerable<IEnumSymbol> Enums => Namespace.Enums;
-        public bool HasEnums => Enums.Any();
-
-        public IEnumerable<IInterfaceSymbol> Interfaces => Namespace.Interfaces;
-        public bool HasInterfaces => Interfaces.Any();
-
-        public IEnumerable<IClassSymbol> Classes => Namespace.Classes;
-        public bool HasClasses => Classes.Any();
-
     }
 }
