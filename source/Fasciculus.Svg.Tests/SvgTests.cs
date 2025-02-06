9f8475e67386eae295aefcd74e1767b40b038c9a
rhjoerg <rhjoerg@gmail.com>
2025-02-06 15:32:04 +0100

    regression

--- source/Fasciculus.Svg.Tests/SvgTests.cs
+++ source/Fasciculus.Svg.Tests/SvgTests.cs
@@ -6,24 +6,28 @@ using System.IO;
 namespace Fasciculus.Svg.Tests
 {
     [TestClass]
-    public class SvgTests
+    public class SvgTests : TestsBase
     {
         [TestMethod]
         public void Test()
         {
-            SvgSvg svg = SvgSvg.Create(0, 0, 160, 80).Width("16rem").Height("8rem").Build();
+            SvgSvg svg = SvgSvg.Create(0, 0, 160, 80).Width("16rem").Height("8rem");
 
-            SvgRect rect1 = SvgRect.Create(10, 10, 40, 20).Fill("red").StrokeWidth("10").Build();
-            SvgRect rect2 = SvgRect.Create(30, 20, 100, 40).Fill("green").StrokeWidth("10").Build();
-            SvgRect rect3 = SvgRect.Create(110, 50, 40, 20).Fill("blue").StrokeWidth("10").Build();
+            SvgRect rect1 = SvgRect.Create(10, 10, 40, 20).Fill("red").Stroke("black").StrokeWidth("2");
+            SvgRect rect2 = SvgRect.Create(30, 20, 100, 40).Fill("green").Stroke("black").StrokeWidth("2");
+            SvgRect rect3 = SvgRect.Create(110, 50, 40, 20).Fill("blue").Stroke("black").StrokeWidth("2");
 
             svg.Add(rect1);
             svg.Add(rect2);
             svg.Add(rect3);
 
+            string asText = svg.ToString();
+
+            Log(asText);
+
             FileInfo file = SpecialDirectories.WorkingDirectory.File("SvgTests.svg");
 
-            file.WriteAllText(svg.ToString());
+            file.WriteAllText(asText);
         }
     }
 }
