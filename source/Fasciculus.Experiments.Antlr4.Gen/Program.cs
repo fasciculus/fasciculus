98456f1d48fe40c0bdb2333911b815e2a33d38e9
rhjoerg <rhjoerg@gmail.com>
2025-02-06 15:27:40 +0100

    wip

---
+++ source/Fasciculus.Experiments.Antlr4.Gen/Program.cs
@@ -0,0 +1,9 @@
+namespace Fasciculus.Experiments.Antlr4.Gen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
--- source/Fasciculus.Experiments.Antlr4/Program.cs
+++ source/Fasciculus.Experiments.Antlr4/Program.cs
@@ -1,21 +1,17 @@
-using Antlr4.Runtime;
-using Fasciculus.Experiments.Calculator;
-using System.Diagnostics;
-
 namespace Fasciculus.Experiments
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            AntlrInputStream input = new("2 + 3");
-            ExprLexer lexer = new(input);
-            CommonTokenStream tokens = new(lexer);
-            ExprParser parser = new(tokens);
+            //AntlrInputStream input = new("2 + 3");
+            //ExprLexer lexer = new(input);
+            //CommonTokenStream tokens = new(lexer);
+            //ExprParser parser = new(tokens);
 
-            var tree = parser.prog();
+            //var tree = parser.prog();
 
-            Debug.WriteLine(tree.ToStringTree());
+            //Debug.WriteLine(tree.ToStringTree());
         }
     }
 }
