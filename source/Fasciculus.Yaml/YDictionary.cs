ae72cd4adff9cb128e75d27684e7427b4e653548
rhjoerg <rhjoerg@gmail.com>
2025-02-09 09:55:51 +0100

    added Fascivulus.Yaml

---
+++ source/Fasciculus.Yaml/YDictionary.cs
@@ -0,0 +1,30 @@
+using System.Collections.Generic;
+using YamlDotNet.Serialization;
+
+namespace Fasciculus.Yaml
+{
+    public class YDictionary
+    {
+        private readonly static IDeserializer Deserializer = new DeserializerBuilder().Build();
+
+        private Dictionary<string, string> entries;
+
+        private YDictionary(Dictionary<string, string> entries)
+        {
+            this.entries = entries;
+        }
+
+        public string GetString(string key, string? defaultValue = null)
+            => entries.TryGetValue(key, out string? value) ? value : defaultValue ?? string.Empty;
+
+        public static YDictionary Deserialize(string data, IDeserializer deserializer)
+        {
+            Dictionary<string, string> entries = deserializer.Deserialize<Dictionary<string, string>>(data);
+
+            return new(entries);
+        }
+
+        public static YDictionary Deserialize(string data)
+            => Deserialize(data, Deserializer);
+    }
+}
