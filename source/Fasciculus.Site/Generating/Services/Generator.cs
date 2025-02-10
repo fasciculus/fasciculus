507de9e365ff4a3b2cb58731d6f661339c830901
rhjoerg <rhjoerg@gmail.com>
2025-02-10 08:34:44 +0100

    integrated WebApplicationDelegate

--- source/Fasciculus.Site/Generating/Services/Generator.cs
+++ source/Fasciculus.Site/Generating/Services/Generator.cs
@@ -1,36 +1,20 @@
 using Fasciculus.Collections;
-using Fasciculus.Reflection;
-using Fasciculus.Threading;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Hosting.Builder;
-using Microsoft.AspNetCore.Hosting.Server;
+using Fasciculus.Web.Extensions;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Http.Features;
-using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 
 namespace Fasciculus.Site.Generating.Services
 {
     public class Generator
     {
-        private readonly RequestDelegate requestDelegate;
-        private readonly IFeatureCollection serverFeatures;
-        private readonly IHttpContextFactory httpContextFactory;
+        private readonly WebApplicationDelegate applicationDelegate;
+
         private readonly GeneratorDocuments documents;
         private readonly GeneratorWriter writer;
 
-        public Generator(IEnumerable<IHostedService> hostedServices, IEnumerable<IStartupFilter> startupFilters,
-            IServer server, IApplicationBuilderFactory applicationBuilderFactory, IHttpContextFactory httpContextFactory,
-            GeneratorDocuments documents, GeneratorWriter writer)
+        public Generator(WebApplicationDelegate applicationDelegate, GeneratorDocuments documents, GeneratorWriter writer)
         {
-            requestDelegate = BuildRequestDelegate(hostedServices, startupFilters, server, applicationBuilderFactory);
-            serverFeatures = server.Features;
-
-            this.httpContextFactory = httpContextFactory;
+            this.applicationDelegate = applicationDelegate;
             this.documents = documents;
             this.writer = writer;
         }
@@ -45,56 +29,9 @@ namespace Fasciculus.Site.Generating.Services
         private void Generate(string document)
         {
             using MemoryStream responseBody = new();
-            FeatureCollection features = CreateFeatures(document, responseBody);
-            HttpContext httpContext = httpContextFactory.Create(features);
+            HttpResponse response = applicationDelegate.Invoke(document, responseBody);
 
-            Tasks.Wait(requestDelegate(httpContext));
-
-            writer.Write(document, httpContext.Response.ContentType ?? string.Empty, responseBody.ToArray());
-        }
-
-        private FeatureCollection CreateFeatures(string document, Stream responseBody)
-        {
-            HttpRequestFeature httpRequestFeature = new()
-            {
-                Protocol = "HTTP/1.1",
-                Method = "GET",
-                Path = document,
-                RawTarget = document
-            };
-
-            HttpResponseFeature httpResponseFeature = new()
-            {
-                Body = responseBody
-            };
-
-            StreamResponseBodyFeature httpResponseBodyFeature = new(responseBody);
-
-            FeatureCollection features = new(serverFeatures);
-
-            features.Set<IHttpRequestFeature>(httpRequestFeature);
-            features.Set<IHttpResponseFeature>(httpResponseFeature);
-            features.Set<IHttpResponseBodyFeature>(httpResponseBodyFeature);
-
-            return features;
-        }
-
-        private static RequestDelegate BuildRequestDelegate(IEnumerable<IHostedService> hostedServices,
-            IEnumerable<IStartupFilter> startupFilters, IServer server, IApplicationBuilderFactory applicationBuilderFactory)
-        {
-            IHostedService webHost = hostedServices.OfType("Microsoft.AspNetCore.Hosting.GenericWebHostService").Single();
-            object options = webHost.GetRequiredProperty<object>("Options");
-            Action<IApplicationBuilder> configure = options.GetRequiredProperty<Action<IApplicationBuilder>>("ConfigureApplication");
-            IApplicationBuilder applicationBuilder = applicationBuilderFactory.CreateBuilder(server.Features);
-
-            foreach (var startupFilter in startupFilters.Reverse())
-            {
-                configure = startupFilter.Configure(configure);
-            }
-
-            configure(applicationBuilder);
-
-            return applicationBuilder.Build();
+            writer.Write(document, response.ContentType ?? string.Empty, responseBody.ToArray());
         }
     }
 }
