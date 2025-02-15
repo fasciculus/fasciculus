97ce72c8696dc87f05cc0588c88092039fcce6ee
rhjoerg <rhjoerg@gmail.com>
2025-02-15 17:45:06 +0100

    added NotNullWhenAttribute

--- source/Fasciculus.NetStandard.Testee/NetStandardTestee.cs
+++ source/Fasciculus.NetStandard.Testee/NetStandardTestee.cs
@@ -4,9 +4,16 @@ namespace Fasciculus.NetStandard.Testee
 {
     public static class NetStandardTestee
     {
-        public static string Foo([DisallowNull] string bar)
+        public static string Foo1([DisallowNull] string bar)
         {
             return bar;
+        }
+
+        public static bool Foo2(string? input, [NotNullWhen(true)] out string? output)
+        {
+            output = input;
+
+            return output is not null;
         }
     }
 }
