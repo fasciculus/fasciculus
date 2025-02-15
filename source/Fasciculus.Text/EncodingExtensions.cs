a1ed6290885a94b4457eff559611e755a095ba36
rhjoerg <rhjoerg@gmail.com>
2025-02-15 17:49:51 +0100

    integrated Fasciculus.NetStandard

--- source/Fasciculus.Text/EncodingExtensions.cs
+++ source/Fasciculus.Text/EncodingExtensions.cs
@@ -11,11 +11,7 @@ namespace Fasciculus.Text
         /// <summary>
         /// Tries to convert the given bytes into a string.
         /// </summary>
-#if NETSTANDARD
-        public static bool TryGetString(this Encoding encoding, byte[] bytes, out string? result)
-#else
         public static bool TryGetString(this Encoding encoding, byte[] bytes, [NotNullWhen(true)] out string? result)
-#endif
         {
             return encoding.TryGetString(bytes, 0, bytes.Length, out result);
         }
@@ -23,11 +19,7 @@ namespace Fasciculus.Text
         /// <summary>
         /// Tries to convert the given bytes (starting from <paramref name="index"/> for <paramref name="count"/> bytes) into a string.
         /// </summary>
-#if NETSTANDARD
-        public static bool TryGetString(this Encoding encoding, byte[] bytes, int index, int count, out string? result)
-#else
         public static bool TryGetString(this Encoding encoding, byte[] bytes, int index, int count, [NotNullWhen(true)] out string? result)
-#endif
         {
             result = null;
 
