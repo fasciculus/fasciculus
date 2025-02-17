7250165c256ac3dc52e80966809025eb6cff84c6
rhjoerg <rhjoerg@gmail.com>
2025-02-17 11:11:42 +0100

    added Fasciculus.NetStandard.Collection

---
+++ source/Fasciculus.NetStandard.Collections/Fasciculus.NetStandard.Enumerable.ToDictionary.cs
@@ -0,0 +1,22 @@
+using System.Collections.Generic;
+
+namespace System.Linq
+{
+    internal static partial class FasciculusNetStandardEnumerableExtensions
+    {
+        internal static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source)
+            where TKey : notnull
+        {
+            return source.ToDictionary(null);
+        }
+
+        internal static Dictionary<TKey, TValue> ToDictionary<TKey, TValue>(this IEnumerable<KeyValuePair<TKey, TValue>> source,
+            IEqualityComparer<TKey>? comparer)
+            where TKey : notnull
+        {
+            comparer ??= EqualityComparer<TKey>.Default;
+
+            return source.ToDictionary(kvp => kvp.Key, kvp => kvp.Value, comparer);
+        }
+    }
+}
---
+++ source/Fasciculus.NetStandard.Collections/README.md
@@ -0,0 +1,7 @@
+# Fasciculus.NetStandard.Attributes
+
+This source-only package contains sources for types not supported by `netstandard2.0`.
+All sources are attributed with `internal`.
+
+## Extensions
+
