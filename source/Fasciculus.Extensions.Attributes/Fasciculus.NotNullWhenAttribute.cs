100fe9c018df78ff6503834895b4eaf7117bdfa0
rhjoerg <rhjoerg@gmail.com>
2025-02-18 18:49:23 +0100

    added Fasciculus.Extensions.Attributes

---
+++ source/Fasciculus.Extensions.Attributes/Fasciculus.NotNullWhenAttribute.cs
@@ -0,0 +1,15 @@
+namespace System.Diagnostics.CodeAnalysis
+{
+    // available in netstandard2.1
+
+    [AttributeUsage(AttributeTargets.Parameter, Inherited = false)]
+    internal sealed class NotNullWhenAttribute : Attribute
+    {
+        public bool ReturnValue { get; }
+
+        public NotNullWhenAttribute(bool returnValue)
+        {
+            ReturnValue = returnValue;
+        }
+    }
+}
---
+++ source/Fasciculus.Extensions.Attributes/README.md
@@ -0,0 +1,9 @@
+# Fasciculus.Extensions.Attributes
+
+This source-only package contains sources for types not supported in older `TargetFramework`s.
+All sources are attributed with `internal`.
+
+## Attributes
+
+- `DisallowNullAttribute`
+- `NotNullWhenAttribute`
