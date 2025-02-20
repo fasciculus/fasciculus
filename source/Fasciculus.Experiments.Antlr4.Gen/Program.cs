667d3c23c8c4ee6017effe90631be6aedcf1b689
rhjoerg <rhjoerg@gmail.com>
2025-02-20 23:01:59 +0100

    progress with nuget

--- source/Fasciculus.Experiments.Antlr4.Gen/Program.cs
+++ source/Fasciculus.Experiments.Antlr4.Gen/Program.cs
@@ -1,9 +1,31 @@
+using Fasciculus.IO;
+using Fasciculus.IO.Searching;
+using Fasciculus.NuGet.Logging;
+using NuGet.Configuration;
+using NuGet.Protocol;
+using NuGet.Protocol.Core.Types;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
 namespace Fasciculus.Experiments.Antlr4.Gen
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            ConsoleLogger logger = new();
+            FileInfo configFile = FileSearch.Search("NuGet.config", SearchPath.WorkingDirectoryAndParents).First();
+            DirectoryInfo configDirectory = configFile.Directory!;
+            ISettings settings = Settings.LoadDefaultSettings(configDirectory.FullName);
+            SettingSection? section = settings.GetSection("config");
+            AddItem? item = section?.GetFirstItemWithAttribute<AddItem>("key", "repositoryPath");
+            string? repositoryPath = item?.Value;
+            DirectoryInfo repositoryDirectory = configDirectory.Combine(repositoryPath!);
+            PackageSource packageSource = new(repositoryDirectory.FullName);
+            SourceRepository repository = Repository.Factory.GetCoreV3(packageSource);
+            FindLocalPackagesResource findLocalPackages = repository.GetResource<FindLocalPackagesResource>();
+            LocalPackageInfo[] packageInfos = [.. findLocalPackages.FindPackagesById("Antlr4.CodeGenerator", logger, CancellationToken.None)];
         }
     }
 }
