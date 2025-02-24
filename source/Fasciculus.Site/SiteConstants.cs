d60101a5860d2bb529700b92f4ef786d90213cb7
rhjoerg <rhjoerg@gmail.com>
2025-02-24 04:04:02 +0100

    added more packages

--- source/Fasciculus.Site/SiteConstants.cs
+++ source/Fasciculus.Site/SiteConstants.cs
@@ -1,6 +1,5 @@
 using NuGet.Frameworks;
 using System.Collections.Generic;
-using static Fasciculus.NuGet.Frameworks.MoreFrameworks;
 using static NuGet.Frameworks.FrameworkConstants.CommonFrameworks;
 
 namespace Fasciculus.Site
@@ -12,15 +11,21 @@ namespace Fasciculus.Site
             "Fasciculus.Algorithms",
             "Fasciculus.Core",
             "Fasciculus.Extensions",
-            "Fasciculus.IO"
+            "Fasciculus.IO",
+            "Fasciculus.Reflection",
+            "Fasciculus.Text",
+            "Fasciculus.Threading",
         };
 
         public static readonly Dictionary<string, NuGetFramework[]> PackageFrameworks = new()
         {
-            { "Fasciculus.Algorithms", [NetStandard20, Net90] },
-            { "Fasciculus.Core", [NetStandard20, Net90] },
-            { "Fasciculus.Extensions", [NetStandard20, Net90] },
-            { "Fasciculus.IO", [NetStandard20, Net90] },
+            { "Fasciculus.Algorithms", [NetStandard20] },
+            { "Fasciculus.Core", [NetStandard20] },
+            { "Fasciculus.Extensions", [NetStandard20] },
+            { "Fasciculus.IO", [NetStandard20] },
+            { "Fasciculus.Reflection", [NetStandard20] },
+            { "Fasciculus.Text", [NetStandard20] },
+            { "Fasciculus.Threading", [NetStandard20] },
         };
     }
 }
