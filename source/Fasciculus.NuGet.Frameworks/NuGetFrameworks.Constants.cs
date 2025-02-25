eb201f2d96893867a367bf4b22b612348c5015bd
rhjoerg <rhjoerg@gmail.com>
2025-02-25 13:58:33 +0100

    added Fasciculus.NuGet.Frameworks

---
+++ source/Fasciculus.NuGet.Frameworks/NuGetFrameworks.Constants.cs
@@ -0,0 +1,10 @@
+using NuGet.Frameworks;
+
+namespace Fasciculus.NuGet.Frameworks
+{
+    public partial class NuGetFrameworks
+    {
+        public static NuGetFramework NetStandard2_0
+            => FrameworkConstants.CommonFrameworks.NetStandard20;
+    }
+}
