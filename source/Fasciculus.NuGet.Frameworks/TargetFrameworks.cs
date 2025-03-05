f683af5952da85b332a46ee6dc659651a80d1d0d
rhjoerg <rhjoerg@gmail.com>
2025-03-05 08:28:00 +0100

    added more frameworks

--- source/Fasciculus.NuGet.Frameworks/TargetFrameworks.cs
+++ source/Fasciculus.NuGet.Frameworks/TargetFrameworks.cs
@@ -1,4 +1,5 @@
 using NuGet.Frameworks;
+using System;
 using System.Collections.Generic;
 using static NuGet.Frameworks.FrameworkConstants;
 
@@ -28,6 +29,16 @@ namespace Fasciculus.NuGet.Frameworks
 
         public static TargetFramework NetStandard_2_1 { get; } = new(CommonFrameworks.NetStandard21);
 
+        public static TargetFramework Net_5_0 { get; } = new(CommonFrameworks.Net50);
+
+        public static TargetFramework Net_6_0 { get; } = new(CommonFrameworks.Net60);
+
+        public static TargetFramework Net_7_0 { get; } = new(CommonFrameworks.Net70);
+
+        public static TargetFramework Net_8_0 { get; } = new(CommonFrameworks.Net80);
+
+        public static TargetFramework Net_9_0 { get; } = new(new(".NETCoreApp", new Version(9, 0, 0, 0)));
+
         public static TargetFramework FromDirectives(IEnumerable<string> directives)
         {
             SortedSet<string> sortedDirectives = [.. directives];
@@ -37,6 +48,23 @@ namespace Fasciculus.NuGet.Frameworks
 
         private static TargetFramework FromDirectives(SortedSet<string> directives)
         {
+            // see https://learn.microsoft.com/en-us/dotnet/standard/frameworks#preprocessor-symbols
+
+            if (directives.Contains("NET9_0")) return Net_9_0;
+            if (directives.Contains("NET9_0_OR_GREATER")) return Net_9_0;
+
+            if (directives.Contains("NET8_0")) return Net_8_0;
+            if (directives.Contains("NET8_0_OR_GREATER")) return Net_8_0;
+
+            if (directives.Contains("NET7_0")) return Net_7_0;
+            if (directives.Contains("NET7_0_OR_GREATER")) return Net_7_0;
+
+            if (directives.Contains("NET6_0")) return Net_6_0;
+            if (directives.Contains("NET6_0_OR_GREATER")) return Net_6_0;
+
+            if (directives.Contains("NET5_0")) return Net_5_0;
+            if (directives.Contains("NET5_0_OR_GREATER")) return Net_5_0;
+
             if (directives.Contains("NETSTANDARD2_1")) return NetStandard_2_1;
             if (directives.Contains("NETSTANDARD2_0")) return NetStandard_2_0;
             if (directives.Contains("NETSTANDARD1_7")) return NetStandard_1_7;
