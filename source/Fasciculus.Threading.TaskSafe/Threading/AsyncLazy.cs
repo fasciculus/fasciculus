64886d9effad6fa6937a52319f8e94c392abb273
rhjoerg <rhjoerg@gmail.com>
2025-03-11 23:38:16 +0100

    simplified AsyncLazy

--- source/Fasciculus.Threading.TaskSafe/Threading/AsyncLazy.cs
+++ source/Fasciculus.Threading.TaskSafe/Threading/AsyncLazy.cs
@@ -10,53 +10,25 @@ namespace Fasciculus.Threading
     public class AsyncLazy<T>
         where T : notnull
     {
-        private readonly Func<T>? factory;
-        private readonly Func<Task<T>>? factoryTask;
-
-        private readonly AsyncMutex mutex = new();
-        private Task<T>? value;
-
         /// <summary>
         /// Gets the lazily initialized value of the current instance.
         /// </summary>
-        public Task<T> Value => GetValue();
+        public Task<T> Value { get; }
 
         /// <summary>
         /// Initializes a new lazy instance with the given <paramref name="factory"/>.
         /// </summary>
         public AsyncLazy(Func<T> factory)
         {
-            this.factory = factory;
+            Value = Tasks.Start(factory);
         }
 
         /// <summary>
-        /// Initializes a new lazy instance with the given <paramref name="factoryTask"/>.
+        /// Initializes a new lazy instance with the given <paramref name="factory"/>.
         /// </summary>
-        public AsyncLazy(Func<Task<T>> factoryTask)
+        public AsyncLazy(Func<Task<T>> factory)
         {
-            this.factoryTask = factoryTask;
-        }
-
-        private Task<T> GetValue()
-        {
-            using AsyncLock asyncLock = Tasks.Wait(AsyncLock.Lock(mutex));
-
-            return value ??= CreateValue();
-        }
-
-        private Task<T> CreateValue()
-        {
-            if (factory is not null)
-            {
-                return Tasks.Start(factory);
-            }
-
-            if (factoryTask is not null)
-            {
-                return Tasks.Start(factoryTask).Unwrap();
-            }
-
-            throw new InvalidOperationException();
+            Value = Tasks.Start(factory).Unwrap();
         }
 
         /// <summary>
