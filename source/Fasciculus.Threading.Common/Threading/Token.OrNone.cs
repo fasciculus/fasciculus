7a9eb652b72015796e74986a6f006c9b35f96c4f
rhjoerg <rhjoerg@gmail.com>
2025-03-12 05:18:55 +0100

    renamed file

--- source/Fasciculus.Threading.Common/Threading/Token.OrNone.cs
+++ source/Fasciculus.Threading.Common/Threading/Token.OrNone.cs
@@ -10,7 +10,7 @@ namespace Fasciculus.Threading
         /// <summary>
         /// Returns the given CancellationToken or <see cref="CancellationToken.None"/> if the given token is <c>null</c>.
         /// </summary>
-        public static CancellationToken OrNone(this CancellationToken? ctk)
-            => ctk ?? CancellationToken.None;
+        public static CancellationToken OrNone(this CancellationToken? cancellationToken)
+            => cancellationToken ?? CancellationToken.None;
     }
 }
